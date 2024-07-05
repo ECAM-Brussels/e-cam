aa11ca4553461e530ce63c9193ac79a306f41d29
Khoi Nguyen <khoi@nguyen.me.uk>
2024-07-05 17:30:39 +0200

    chore(web): set up file based routing

--- src/app.tsx
+++ src/app.tsx
@@ -1,21 +1,11 @@
-import { createSignal } from 'solid-js'
+import { Router } from '@solidjs/router'
+import { FileRoutes } from '@solidjs/start/router'
+import { Suspense } from 'solid-js'
 
 export default function App() {
-  const [count, setCount] = createSignal(0)
-
   return (
-    <main>
-      <h1>Hello world!</h1>
-      <button class="increment" onClick={() => setCount(count() + 1)} type="button">
-        Clicks: {count()}
-      </button>
-      <p>
-        Visit{' '}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{' '}
-        to learn how to build SolidStart apps.
-      </p>
-    </main>
+    <Router root={(props) => <Suspense>{props.children}</Suspense>}>
+      <FileRoutes />
+    </Router>
   )
 }
