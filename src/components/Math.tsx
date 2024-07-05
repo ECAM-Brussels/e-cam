4a8473d84b36dc4b7a35e7ebe0063089fae472c0
Khoi Nguyen <khoi@nguyen.me.uk>
2024-07-05 12:49:12 +0200

    feat(web): basic Math component

---
+++ src/components/Math.tsx
@@ -0,0 +1,11 @@
+import katex from "katex";
+import "katex/dist/katex.min.css";
+
+type MathProps = {
+  value: string;
+};
+
+export default function Math(props: MathProps) {
+  const html = () => katex.renderToString(props.value);
+  return <span innerHTML={html()} />;
+}
