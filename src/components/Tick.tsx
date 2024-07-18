f816481c62c7c823dd84499fa62183a264500515
Khoi Nguyen <khoi@nguyen.me.uk>
2024-07-18 15:10:02 +0200

    feat(ui): tick component to mark

---
+++ src/components/Tick.tsx
@@ -0,0 +1,20 @@
+import { faCheck, faXmark } from '@fortawesome/free-solid-svg-icons'
+import { Show } from 'solid-js'
+import Fa from '~/components/Fa'
+
+export default function Tick(props: { value: boolean | undefined }) {
+  return (
+    <>
+      <Show when={props.value === true}>
+        <span class="text-green-700">
+          <Fa icon={faCheck} />
+        </span>
+      </Show>
+      <Show when={props.value === false}>
+        <span class="text-red-700">
+          <Fa icon={faXmark} />
+        </span>
+      </Show>
+    </>
+  )
+}
