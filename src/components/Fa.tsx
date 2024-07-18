965d4988012404b505b593fa4e7f2a30d7a0e81d
Khoi Nguyen <khoi@nguyen.me.uk>
2024-07-18 15:08:22 +0200

    feat(ui): set up font awesome

---
+++ src/components/Fa.tsx
@@ -0,0 +1,20 @@
+import type { IconDefinition } from '@fortawesome/fontawesome-common-types'
+
+interface FaProps {
+  icon: IconDefinition
+}
+
+export default function Fa(props: FaProps) {
+  const i = () => props.icon.icon
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox={`0 0 ${i()[0]} ${i()[1]}`}
+      fill="currentColor"
+      class="icon"
+      height="1em"
+    >
+      <path d={i()[4] as string} />
+    </svg>
+  )
+}
