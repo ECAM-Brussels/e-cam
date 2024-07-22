4a162e2be6acf21deae438dd7ed641ab8905f2d7
Khoi Nguyen <khoi@nguyen.me.uk>
2024-07-22 15:18:22 +0200

    chore(test): test equation

---
+++ src/exercises/Equation.test.tsx
@@ -0,0 +1,7 @@
+import { mark } from './Equation'
+import { test, expect } from 'vitest'
+
+test('marks correctly', async () => {
+  expect(await mark({ equation: 'x^2-5x+6', attempt: ['2', '3']})).toBe(true)
+  expect(await mark({ equation: 'x^2 - 1', attempt: ['-1', '1'] })).toBe(true)
+})
