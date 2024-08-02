af0ba9caf49f732c00b3d5a921944ad81761ac5c
Khoi Nguyen <khoi@nguyen.me.uk>
2024-08-02 13:29:27 +0200

    feat(assignments): show results to admin

--- src/vite/assignment.tsx
+++ src/vite/assignment.tsx
@@ -1,7 +1,9 @@
-import { type RouteDefinition } from '@solidjs/router'
+import { createAsync, useLocation, useSearchParams, type RouteDefinition } from '@solidjs/router'
+import { Show } from 'solid-js'
 import { createStore } from 'solid-js/store'
 import ExerciseSequence, { loadAssignment, type Exercise } from '~/components/ExerciseSequence'
 import Page from '~/components/Page'
+import Results from '~/components/Results'
 import { getUser } from '~/lib/auth/session'
 
 export const route = {
@@ -14,9 +16,17 @@ export const route = {
 export default function () {
   // @ts-ignore
   const [data, setData] = createStore<Exercise[]>($body$)
+  const location = useLocation()
+  const user = createAsync(() => getUser())
+  const [searchParams, setSearchParams] = useSearchParams()
   return (
     <Page>
-      <ExerciseSequence data={data} setter={setData} />
+      <Show
+        when={searchParams.results && user()?.admin}
+        fallback={<ExerciseSequence data={data} setter={setData} />}
+      >
+        <Results url={location.pathname} />
+      </Show>
     </Page>
   )
 }
