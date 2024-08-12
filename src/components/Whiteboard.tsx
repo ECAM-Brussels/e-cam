0c20993b2427ec4c84dc8c419c8863459c5b78c9
Khoi Nguyen <khoi@nguyen.me.uk>
2024-08-12 22:12:29 +0200

    feat(whiteboard): mostly functioning

--- src/components/Whiteboard.tsx
+++ src/components/Whiteboard.tsx
@@ -1,59 +1,187 @@
-import { createEffect, createSignal } from 'solid-js'
-import { type SetStoreFunction } from 'solid-js/store'
+import { cache, createAsync, revalidate, useLocation } from '@solidjs/router'
+import { throttle } from 'lodash-es'
+import { createEffect, createSignal, on, onMount } from 'solid-js'
+import { createStore } from 'solid-js/store'
+import { getUser } from '~/lib/auth/session'
+import { prisma } from '~/lib/db'
 
 type Mode = 'draw' | 'erase' | 'read'
 
 type Stroke = {
   color: string
   lineWidth: number
-  points: [number, number]
+  points: [number, number][]
 }
 
+export const loadBoard = cache(async (url: string, id: string) => {
+  'use server'
+  const record = await prisma.board.findUnique({ where: { url_id: { url, id } } })
+  return record ? (JSON.parse(String(record.body)) as Stroke[]) : null
+}, 'loadBoards')
+
+const upsertBoard = throttle(async (url: string, id: string, data: Stroke[]) => {
+  'use server'
+  const user = await getUser()
+  if (!user || !user.admin) {
+    throw new Error('Error when upserting board: user is not an admin')
+  }
+  const body = JSON.stringify(data)
+  await prisma.board.upsert({
+    where: { url_id: { url, id } },
+    update: { body, lastModified: new Date() },
+    create: { url, id, body },
+  })
+  revalidate(loadBoard.keyFor(url, id))
+}, 5000)
+
 type WhiteboardProps = {
+  id?: string
+  class?: string
   height: number
-  readOnly: boolean
-  setter: SetStoreFunction<Stroke[]>
-  strokes: Stroke[]
+  readOnly?: boolean
   width: number
 }
 
 export default function Whiteboard(props: WhiteboardProps) {
-  const [mode, setMode] = createSignal<Mode>('draw')
-  const index = () => props.strokes.length - 1
-  const lastStroke = () => props.strokes[index()]
+  const location = useLocation()
+  let canvasRef: HTMLCanvasElement
+  let container: HTMLDivElement
+  const ctx = () => canvasRef?.getContext('2d')
+  const [mode, setMode] = createSignal<Mode>('read')
 
-  const handlePointerMotion = (x: number, y: number) => {
+  const [strokes, setStrokes] = createStore<Stroke[]>([])
+  const savedStrokes = createAsync(() => loadBoard(location.pathname, props.id || ''))
+  createEffect(() => {
+    const saved = savedStrokes()
+    if (saved) {
+      setStrokes(saved)
+    }
+  })
+
+  const [currentStroke, setCurrentStroke] = createStore<Stroke>({
+    color: '#255994',
+    lineWidth: 1,
+    points: [],
+  })
+
+  const handlePointerMove = (x: number, y: number) => {
     if (mode() === 'draw') {
-      props.setter(index(), 'points', [...lastStroke().points, [x, y]])
+      const lastPoint = currentStroke.points[currentStroke.points.length - 1]
+      if (!lastPoint || lastPoint[0] !== x || lastPoint[1] !== y) {
+        setCurrentStroke('points', currentStroke.points.length, [x, y])
+      }
+    } else if (mode() === 'erase') {
+      for (let i = 0; i < strokes.length; i++) {
+        for (const p of strokes[i].points) {
+          const dist = (p[0] - x) ** 2 + (p[1] - y) ** 2
+          if (dist <= 5) {
+            setStrokes(strokes.filter((s, j) => j !== i))
+            return
+          }
+        }
+      }
     }
   }
 
   createEffect(() => {
-    // Redraw strokes
+    const context = ctx()
+    if (context) {
+      context.fillStyle = currentStroke.color
+      context.strokeStyle = currentStroke.color
+      context.lineCap = 'round'
+      context.lineJoin = 'round'
+      context.lineWidth = currentStroke.lineWidth
+    }
+  })
+
+  createEffect(
+    on(mode, () => {
+      if (mode() === 'read' && currentStroke.points) {
+        setStrokes(strokes.length, currentStroke)
+        setCurrentStroke({ ...currentStroke, points: [] })
+      }
+    }),
+  )
+
+  // Drawing strokes from scratch
+  createEffect(
+    on(
+      () => strokes,
+      () => {
+        const context = ctx()!
+        context.clearRect(0, 0, props.width, props.height)
+        for (const stroke of strokes) {
+          context.beginPath()
+          context.fillStyle = stroke.color
+          context.strokeStyle = stroke.color
+          context.lineWidth = stroke.lineWidth
+          for (const point of stroke.points) {
+            context.lineTo(...point)
+          }
+          context.stroke()
+          context.closePath()
+        }
+        context.fillStyle = currentStroke.color
+        context.strokeStyle = currentStroke.color
+        context.lineWidth = currentStroke.lineWidth
+        upsertBoard(location.pathname, props.id || '', strokes)
+      },
+    ),
+  )
+
+  // Adding points
+  createEffect(
+    on(
+      () => currentStroke.points.length,
+      () => {
+        if (currentStroke.points.length < 2) {
+          return
+        }
+        const context = ctx()!
+        const lastPoint = (i: number) => currentStroke.points[currentStroke.points.length - i]
+        context.moveTo(...lastPoint(2))
+        context.lineTo(...lastPoint(1))
+        context.stroke()
+      },
+    ),
+  )
+
+  onMount(() => {
+    canvasRef!.oncontextmenu = () => false
   })
 
   return (
-    <canvas
-      height={props.height}
-      width={props.width}
-      onClick={(event) => {
-        if (props.readOnly) {
-          return
-        }
-        const leftClick = event.button === 0 || event.button === 1
-        const rightClick = event.button === 2
-        if (rightClick) {
-          setMode('erase')
-        } else {
-          setMode('draw')
-        }
-      }}
-      onMouseMove={(event) => {
-        handlePointerMotion(event.clientX, event.clientY)
-      }}
-      onMouseUp={(event) => {
-        setMode('read')
-      }}
-    />
+    <div ref={container!} class="relative" oncontextmenu={() => false}>
+      <canvas
+        class={`absolute top-0 left-0 cursor-crosshair ${props.class}`}
+        ref={canvasRef!}
+        height={props.height}
+        width={props.width}
+        onMouseDown={(event) => {
+          if (props.readOnly) {
+            return
+          }
+          const leftClick = event.button === 0 || event.button === 1
+          const rightClick = event.button === 2
+          if (rightClick) {
+            setMode('erase')
+          } else if (leftClick) {
+            setMode('draw')
+          }
+        }}
+        onMouseMove={(event) => {
+          const boundingClientRect = canvasRef.getBoundingClientRect()
+          const scaleX = canvasRef.offsetWidth / boundingClientRect.width
+          const scaleY = canvasRef.offsetHeight / boundingClientRect.height
+          const containerClient = container.getBoundingClientRect()
+          const x = Math.round((event.clientX - containerClient.left) * scaleX)
+          const y = Math.round((event.clientY - containerClient.top) * scaleY)
+          handlePointerMove(x, y)
+        }}
+        onMouseUp={() => {
+          setMode('read')
+        }}
+      />
+    </div>
   )
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,12 @@
-import { type RouteDefinition } from '@solidjs/router'
-import { createStore } from 'solid-js/store'
-import ExerciseSequence, { loadAssignment, type Exercise as ExerciseSchema } from '~/components/ExerciseSequence'
 import Page from '~/components/Page'
+import Whiteboard from '~/components/Whiteboard'
 
 export default function Home() {
   return (
     <Page>
       <section>
         <h2>Une plateforme d'apprentissage</h2>
+        <Whiteboard class="border-2 border-red-300" width={800} height={800} />
       </section>
     </Page>
   )
