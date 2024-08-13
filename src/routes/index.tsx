0150a148ad98ac97d924b2e3783dbe4e0a7017f4
Khoi Nguyen <khoi@nguyen.me.uk>
2024-08-13 20:37:43 +0200

    chore(homepage): remove whiteboard

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,10 @@
 import Page from '~/components/Page'
-import Whiteboard from '~/components/Whiteboard'
 
 export default function Home() {
   return (
     <Page>
       <section>
         <h2>Une plateforme d'apprentissage</h2>
-        <Whiteboard class="border-2 border-red-300" width={800} height={800} />
       </section>
     </Page>
   )
