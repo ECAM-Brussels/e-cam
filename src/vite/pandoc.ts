4b3d08dd517c2284a67d923c210ff5a70dbea805
Khoi Nguyen <khoi@nguyen.me.uk>
2024-08-16 12:56:24 +0200

    chore(vite): improve dx

--- src/vite/pandoc.ts
+++ src/vite/pandoc.ts
@@ -7,52 +7,68 @@ import { type Plugin } from 'vite'
 
 const exec = promisify(execWithCallback)
 
+async function generatePage(file: string) {
+  const relativePath = relative(resolve('content'), file)
+  const outputPath = resolve('src/routes/(generated)', relativePath.replace(/\.md$/, '.tsx'))
+  mkdirSync(dirname(outputPath), { recursive: true })
+
+  const metaFile = `${outputPath}.json`
+  await exec(`pandoc "${file}" -t html5 -o "${metaFile}" --template src/vite/template.json.txt`)
+  const meta = JSON.parse(readFileSync(metaFile, 'utf-8'))
+  const template = meta.slideshow ? 'template.slideshow.tsx' : 'template.tsx'
+
+  let cmd = [
+    `pandoc "${file}"`,
+    `-o "${outputPath}"`,
+    '-t html5',
+    `--template src/vite/${template}`,
+    '--wrap=preserve',
+  ]
+
+  const filters = await glob.glob('src/vite/filters/*.py')
+  for (const filter of filters) {
+    cmd.push(`--filter ${filter}`)
+  }
+
+  try {
+    const { stderr } = await exec(cmd.join(' '))
+    if (stderr) {
+      console.log(`Error converting ${file} to ${outputPath}:`, stderr)
+    }
+  } catch (error) {
+    console.error('Error while converting with pandoc:', error)
+  }
+}
+
+async function createAssignment(file: string) {
+  const relativePath = relative(resolve('content'), file)
+  const outputPath = resolve('src/routes/(generated)', relativePath.replace(/\.ts$/, '.tsx'))
+  mkdirSync(dirname(outputPath), { recursive: true })
+  const data = String(readFileSync(file, 'utf-8'))
+  const template = String(readFileSync(resolve('src/vite/assignment.tsx'), 'utf-8'))
+  const content = template.replace('$body$', data)
+  writeFileSync(outputPath, content, 'utf-8')
+}
+
 const pandocPlugin = (): Plugin => {
   return {
     name: 'pandoc-plugin',
     async handleHotUpdate({ file }) {
+      if (file.startsWith(resolve('src/vite'))) {
+        const pages = await glob.glob('content/**/*.md')
+        for (const file of pages) {
+          generatePage(file)
+        }
+        const assignments = await glob.glob('content/**/*.ts')
+        for (const file of assignments) {
+          createAssignment(file)
+        }
+      }
       if (file.endsWith('.ts') && file.startsWith(resolve('content'))) {
-        const relativePath = relative(resolve('content'), file)
-        const outputPath = resolve('src/routes/(generated)', relativePath.replace(/\.ts$/, '.tsx'))
-        mkdirSync(dirname(outputPath), { recursive: true })
-        const data = String(readFileSync(file, 'utf-8'))
-        const template = String(readFileSync(resolve('src/vite/assignment.tsx'), 'utf-8'))
-        const content = template.replace('$body$', data)
-        writeFileSync(outputPath, content, 'utf-8')
+        createAssignment(file)
       }
       if (file.endsWith('.md') && file.startsWith(resolve('content'))) {
-        const relativePath = relative(resolve('content'), file)
-        const outputPath = resolve('src/routes/(generated)', relativePath.replace(/\.md$/, '.tsx'))
-        mkdirSync(dirname(outputPath), { recursive: true })
-
-        const metaFile = `${outputPath}.json`
-        await exec(
-          `pandoc "${file}" -t html5 -o "${metaFile}" --template src/vite/template.json.txt`,
-        )
-        const meta = JSON.parse(readFileSync(metaFile, 'utf-8'))
-        const template = meta.slideshow ? 'template.slideshow.tsx' : 'template.tsx'
-
-        let cmd = [
-          `pandoc "${file}"`,
-          `-o "${outputPath}"`,
-          '-t html5',
-          `--template src/vite/${template}`,
-          '--wrap=preserve',
-        ]
-
-        const filters = await glob.glob('src/vite/filters/*.py')
-        for (const filter of filters) {
-          cmd.push(`--filter ${filter}`)
-        }
-
-        try {
-          const { stderr } = await exec(cmd.join(' '))
-          if (stderr) {
-            console.log(`Error converting ${file} to ${outputPath}:`, stderr)
-          }
-        } catch (error) {
-          console.error('Error while converting with pandoc:', error)
-        }
+        generatePage(file)
       }
     },
   }
