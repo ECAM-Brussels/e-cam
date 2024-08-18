db633013c6199fbf2cf96fff91e0562a20b0a1cb
Khoi Nguyen <khoi@nguyen.me.uk>
2024-08-18 17:37:54 +0200

    feat(pandoc): add geogebra component

---
+++ src/components/Geogebra.tsx
@@ -0,0 +1,29 @@
+import { mergeProps } from 'solid-js'
+
+interface GeogebraProps {
+  class?: string
+  classList?: { [className: string]: boolean }
+  height?: number
+  id: string
+  width?: number
+}
+
+export default function Geogebra(props: GeogebraProps) {
+  props = mergeProps({ height: 600, width: 900 }, props)
+  const src = () => {
+    let result = `https://www.geogebra.org/material/iframe/id/${props.id}`
+    result += `/width/${props.width}/height/${props.height}`
+    result += `/ai/false/smb/false/stb/false`
+    return result
+  }
+  return (
+    <iframe
+      classList={{ 'z-20': true, ...props.classList }}
+      class={props.class}
+      src={src()}
+      height={props.height}
+      width={props.width}
+      allowfullscreen
+    />
+  )
+}
