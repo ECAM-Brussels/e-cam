1bb93e74712b542d8e0e33c1f2a3234f4119b4c6
Khoi Nguyen <khoi@nguyen.me.uk>
2024-08-20 12:44:25 +0200

    chore(ui): home page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,37 @@
+import { type JSXElement } from 'solid-js'
 import Page from '~/components/Page'
 
 export default function Home() {
   return (
     <Page>
       <section>
-        <h2>Une plateforme d'apprentissage</h2>
+        <h2 class="text-3xl text-slate-800 mb-4">Cours</h2>
+        <div class="grid grid-cols-2">
+          <Card src="images/PM1C.webp" alt="Pont mathématiques" href="/PM1C/">
+            <h3>Pont mathématiques vers le supérieur (ba1)</h3>
+          </Card>
+        </div>
       </section>
     </Page>
+  )
+}
+
+type CardProps = {
+  alt: string
+  children?: JSXElement
+  src: string
+  href: string
+}
+
+function Card(props: CardProps) {
+  return (
+    <a href={props.href}>
+      <div class="bg-white rounded-xl shadow">
+        <img src={props.src} alt={props.alt} class="rounded-t opacity-60 max-h-64 object-cover w-full" />
+        <div class="px-2 py-4 prose">
+          {props.children}
+        </div>
+      </div>
+    </a>
   )
 }
