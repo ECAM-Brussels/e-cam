9f44b1f86dbf3974db3d9b770193fced5acff71b
Khoi Nguyen <khoi@nguyen.me.uk>
2024-08-23 14:38:56 +0200

    chore(api): improve sytem generator

--- symapi/system.py
+++ symapi/system.py
@@ -1,6 +1,7 @@
 import random
 import strawberry
 import sympy
+from typing import Optional
 
 from symapi.core import Math
 
@@ -15,6 +16,7 @@ class System:
         Lentries: list[int],
         Uentries: list[int],
         X: list[int],
+        pivot_count: Optional[int],
     ) -> list[Math]:
         n = len(variables)
         L = sympy.zeros(n, n)
@@ -23,9 +25,13 @@ class System:
             L[i, i] = 1
             for j in range(n):
                 if j < i:
-                    L[i, j] = random.choice(Lentries)
+                    if pivot_count is None or pivot_count > 0:
+                        L[i, j] = random.choice(Lentries)
+                    if pivot_count is not None:
+                        pivot_count -= 1
                 else:
                     U[i, j] = random.choice(Uentries)
+                    U[i, j] = 1 if i == j and U[i, j] == 0 else U[i, j]
 
         A = L * U
         x = sympy.Matrix([random.choice(X) for _ in range(n)])
