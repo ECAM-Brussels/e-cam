e581d57ecc3552fc9e4ae879c156757b3ea4f0a9
Khoi Nguyen <khoi@nguyen.me.uk>
2024-08-24 18:00:41 +0200

    chore(code): use classList for tailwind

--- src/components/Slide.tsx
+++ src/components/Slide.tsx
@@ -2,6 +2,7 @@ import { type JSXElement } from 'solid-js'
 
 type SlideProps = {
   children?: JSXElement
+  classList?: { [cls: string]: boolean }
   class: string
   title: JSXElement
 }
@@ -10,7 +11,20 @@ export default function Slide(props: SlideProps) {
   return (
     <div class="h-full">
       <div class="bg-slate-700 mb-6 px-4 py-3 text-slate-100 text-left text-3xl">{props.title}</div>
-      <div class={`prose prose-2xl px-4 max-w-none text-left ${props.class}`}>{props.children}</div>
+      <div
+        class={props.class}
+        classList={{
+          'h-full': true,
+          prose: true,
+          'prose-2xl': true,
+          'px-4': true,
+          'text-left': true,
+          'max-w-none': true,
+          ...props.classList,
+        }}
+      >
+        {props.children}
+      </div>
     </div>
   )
 }
