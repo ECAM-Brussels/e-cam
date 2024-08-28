12bebc88dca0a33a35a0312283072aea05dfc755
Khoi Nguyen <khoi@nguyen.me.uk>
2024-08-28 11:39:57 +0200

    chore(ui): improve calculator style

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,3 +1,5 @@
+import Fa from './Fa'
+import { faCalculator } from '@fortawesome/free-solid-svg-icons'
 import { cache } from '@solidjs/router'
 import { createResource, createSignal, Suspense } from 'solid-js'
 import Math from '~/components/Math'
@@ -39,17 +41,22 @@ export default function Calculator(props: CalculatorProps) {
   const [answer] = createResource(prompt, calculate)
   return (
     <div class={props.class} classList={{ 'my-8': true }}>
-      <Math
-        class="border rounded-xl w-full relative z-20"
-        value={prompt()}
-        onBlur={(e) => setPrompt(e.target.value)}
-        onkeydown={(e: KeyboardEvent) => {
-          if (e.key === 'Enter') {
-            ;(e.target as HTMLInputElement).blur()
-          }
-        }}
-        editable
-      />
+      <div class="flex items-center gap-4 mx-8">
+        <button class="border border-sky-800 text-sky-800 px-4 py-1 rounded-xl relative z-20">
+          <Fa icon={faCalculator} />
+        </button>
+        <Math
+          class="border rounded-xl w-full relative z-20 shadow px-4 py-2"
+          value={prompt()}
+          onBlur={(e) => setPrompt(e.target.value)}
+          onkeydown={(e: KeyboardEvent) => {
+            if (e.key === 'Enter') {
+              ;(e.target as HTMLInputElement).blur()
+            }
+          }}
+          editable
+        />
+      </div>
       <Suspense fallback={<Spinner />}>
         <Math value={answer()} displayMode />
       </Suspense>
