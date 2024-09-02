e9957b75509301d7f34d9f23325adec14cda41f6
Khoi Nguyen <khoi@nguyen.me.uk>
2024-09-02 12:58:15 +0200

    feat(code): jupyter-like reexecution

--- src/components/Code.tsx
+++ src/components/Code.tsx
@@ -1,9 +1,11 @@
+import { faPlayCircle } from '@fortawesome/free-solid-svg-icons'
 import { clientOnly } from '@solidjs/start'
-import { Show } from 'solid-js'
+import { createEffect, createSignal, Show } from 'solid-js'
 import 'solid-prism-editor/layout.css'
 import 'solid-prism-editor/prism/languages/common'
 import 'solid-prism-editor/search.css'
 import 'solid-prism-editor/themes/github-light.css'
+import Fa from '~/components/Fa'
 import Html from '~/components/Html'
 
 type CodeProps = {
@@ -19,14 +21,45 @@ const Editor = clientOnly(async () => {
 const Python = clientOnly(() => import('./Python'))
 
 export default function Code(props: CodeProps) {
+  const [value, setValue] = createSignal(props.value)
+  createEffect(() => {
+    setValue(props.value)
+  })
+
+  let textarea: HTMLTextAreaElement
+
   return (
     <div class="border rounded-xl m-8 shadow relative z-20">
-      <Editor language={props.lang} value={props.value} />
+      <div class="flex items-end">
+        <button
+          class="block text-cyan-950 px-2 text-xl"
+          onClick={() => {
+            setValue(textarea.value)
+          }}
+        >
+          <Fa icon={faPlayCircle} />
+        </button>
+        <div class="w-full">
+          <Editor
+            language={props.lang}
+            value={value()}
+            onMount={(editor) => {
+              textarea = editor.textarea
+              textarea.addEventListener('keydown', (event) => {
+                if (event.code === 'Enter' && (event.shiftKey || event.ctrlKey)) {
+                  event.preventDefault()
+                  setValue(textarea.value)
+                }
+              })
+            }}
+          />
+        </div>
+      </div>
       <Show when={props.lang === 'python' && props.run}>
-        <Python value={props.value} />
+        <Python value={value()} />
       </Show>
       <Show when={props.lang === 'html' && props.run}>
-        <Html value={props.value} />
+        <Html value={value()} />
       </Show>
     </div>
   )
