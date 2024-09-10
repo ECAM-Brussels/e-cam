5a3a6eb574d16c976a3f153015f42e7e58b86345
Khoi Nguyen <khoi@nguyen.me.uk>
2024-09-10 20:49:14 +0200

    chore(boards): use wss instead of crossws

--- src/routes/api/boards.ts
+++ src/routes/api/boards.ts
@@ -1,36 +1,18 @@
 import type { APIEvent } from '@solidjs/start/server'
-import crossws from 'crossws/adapters/node'
+import { WebSocketServer } from 'ws'
 
-const websocket = crossws({
-  hooks: {
-    open(peer) {
-      console.log('[ws] open', peer)
-      peer.subscribe('boards')
-    },
-
-    message(peer, message) {
-      peer.publish('boards', message.text())
-      peer.send(message.text())
-    },
-
-    close(peer, event) {
-      console.log('[ws] close', peer, event)
-    },
-
-    error(peer, error) {
-      console.log('[ws] error', peer, error)
-    },
-
-    upgrade(req) {
-      console.log(`[ws] upgrading ${req.url}...`)
-      return {
-        headers: {},
-      }
-    },
-  },
+const wss = new WebSocketServer({ noServer: true })
+wss.on('connection', (ws) => {
+  ws.on('message', (message) => {
+    wss.clients.forEach((client) => {
+      client.send(String(message))
+    })
+  })
 })
 
 export function GET(event: APIEvent) {
   const request = event.nativeEvent.node.req
-  websocket.handleUpgrade(request, request.socket, Buffer.from(''))
+  wss.handleUpgrade(request, request.socket, Buffer.from(''), (ws) => {
+    wss.emit('connection', ws, request)
+  })
 }
