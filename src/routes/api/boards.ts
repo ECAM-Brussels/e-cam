d1801e52c8e1f2a9b204c0685f65ee9cef15655d
Khoi Nguyen <khoi@nguyen.me.uk>
2024-09-10 20:09:50 +0200

    chore(boards): simplify socket code

--- src/routes/api/boards.ts
+++ src/routes/api/boards.ts
@@ -1,6 +1,5 @@
 import type { APIEvent } from '@solidjs/start/server'
 import crossws from 'crossws/adapters/node'
-import type { IncomingMessage } from 'node:http'
 
 const websocket = crossws({
   hooks: {
@@ -31,22 +30,7 @@ const websocket = crossws({
   },
 })
 
-const emptyBuffer = Buffer.from('')
-const handleUpgrade = (request: IncomingMessage) =>
-  websocket.handleUpgrade(request, request.socket, emptyBuffer)
-
-const isWsConnect = ({ headers }: IncomingMessage) =>
-  headers['connection']?.toLowerCase().includes('upgrade') &&
-  headers['upgrade'] === 'websocket' &&
-  headers['sec-websocket-version'] === '13' &&
-  typeof headers['sec-websocket-key'] === 'string'
-
 export function GET(event: APIEvent) {
   const request = event.nativeEvent.node.req
-  if (!isWsConnect(request))
-    return new Response(undefined, { status: 400, statusText: 'Bad Request' })
-
-  handleUpgrade(request)
-
-  return undefined
+  websocket.handleUpgrade(request, request.socket, Buffer.from(''))
 }
