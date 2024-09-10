b1391a5c9842ec0712be9fb1668fbe82f6363d25
Khoi Nguyen <khoi@nguyen.me.uk>
2024-09-10 13:00:33 +0200

    fix(symapi): allow \\sqrt3 because of mathlive

--- symapi/core.py
+++ symapi/core.py
@@ -1,3 +1,4 @@
+import re
 import strawberry
 import sympy
 import sympy.core.function
@@ -6,6 +7,7 @@ from typing import NewType
 
 
 def parse_latex(expr: str):
+    expr = re.sub(r"\\sqrt(\d+)", r"\\sqrt{\1}", expr)
     if "=" in expr:
         return sympy.Eq(*[parse_latex(s) for s in expr.split("=")])
     parsed = sympy.parsing.latex.parse_latex(expr)
@@ -22,16 +24,17 @@ def parse_latex(expr: str):
 
     return remove_funcs(parsed) if parsed is not None else None
 
+
 def remove_funcs(expr: sympy.Basic) -> sympy.Basic:
     if isinstance(expr.func, sympy.core.function.UndefinedFunction):
         x = sympy.Symbol(str(expr.func))
-        print('x:', x, expr.args)
         return sympy.Mul(x, *expr.args, evaluate=False)
     if expr.func and expr.args:
         with sympy.evaluate(False):
             args = [remove_funcs(a) for a in expr.args]
             return expr.func(*args)
     return expr
+
 
 Math = strawberry.scalar(
     NewType("Math", sympy.Basic),
