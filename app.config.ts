1b6093e3befc051f62606f976333bb45d40abfa7
Khoi Nguyen <khoi@nguyen.me.uk>
2024-09-11 10:14:00 +0200

    fix(nitro): enable websocket support

--- app.config.ts
+++ app.config.ts
@@ -2,6 +2,11 @@ import pandocPlugin from './src/vite/pandoc'
 import { defineConfig } from '@solidjs/start/config'
 
 export default defineConfig({
+  server: {
+    experimental: {
+      websocket: true,
+    }
+  },
   vite: {
     plugins: [pandocPlugin()],
     server: {
