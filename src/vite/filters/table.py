919ab0265bade1679a62625ead3c85e79a43cba6
Khoi Nguyen <khoi@nguyen.me.uk>
2024-09-13 14:30:25 +0200

    chore(pandoc): improve table style

---
+++ src/vite/filters/table.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import panflute as pf
+
+def table(el: pf.Element, doc: pf.Doc):
+    if isinstance(el, pf.Table):
+        el.classes += ['container']
+    if isinstance(el, pf.TableRow):
+        el.classes += ['even:bg-slate-50']
+
+pf.run_filter(table)
