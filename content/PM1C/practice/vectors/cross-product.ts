7bceeebb83ab4a987a630d4b384ef182f6abcfe2
Khoi Nguyen <khoi@nguyen.me.uk>
2024-09-17 22:00:23 +0200

    content(pm1c): fix difficulty

--- content/PM1C/practice/vectors/cross-product.ts
+++ content/PM1C/practice/vectors/cross-product.ts
@@ -5,12 +5,6 @@
     {
       type: 'CrossProduct',
       params: {
-        numbers: Array.from({ length: 20 }, (_, i) => String(i - 10)).concat('a', 'b', 'c', 'x', 'y')
-      },
-    },
-    {
-      type: 'CrossProduct',
-      params: {
         numbers: ['0', '1', '2', '3', '4'],
       },
     },
@@ -18,6 +12,12 @@
       type: 'CrossProduct',
       params: {
         numbers: Array.from({ length: 20 }, (_, i) => String(i - 10)),
+      },
+    },
+    {
+      type: 'CrossProduct',
+      params: {
+        numbers: Array.from({ length: 20 }, (_, i) => String(i - 10)).concat('a', 'b', 'c', 'x', 'y')
       },
     },
   ],
