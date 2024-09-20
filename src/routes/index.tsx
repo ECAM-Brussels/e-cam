156d1cdce7facd2addf53b66e99ae9016cabbae3
Khoi Nguyen <khoi@nguyen.me.uk>
2024-09-20 11:15:18 +0200

    content(homepage): add link to python course

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,9 +6,12 @@ export default function Home() {
     <Page>
       <section>
         <h2 class="text-3xl text-slate-800 mb-4">Cours</h2>
-        <div class="grid lg:grid-cols-2">
+        <div class="grid lg:grid-cols-2 gap-8">
           <Card src="images/PM1C.png" alt="Pont mathématiques" href="/PM1C/">
             <h3>Pont mathématiques vers le supérieur (ba1)</h3>
+          </Card>
+          <Card src="images/IC1T.png" alt="Programmation" href="/IC1T/">
+            <h3>Programmation</h3>
           </Card>
         </div>
       </section>
@@ -30,7 +33,7 @@ function Card(props: CardProps) {
         <img
           src={props.src}
           alt={props.alt}
-          class="rounded-t opacity-60 max-h-96 object-cover w-full"
+          class="rounded-t opacity-60 h-96 object-cover w-full"
         />
         <div class="px-2 py-4 prose">{props.children}</div>
       </div>
