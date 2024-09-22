52b0a9c542cb192cf981bd7ac4b08144a7cd0f68
Khoi Nguyen <khoi@nguyen.me.uk>
2024-09-22 10:42:29 +0200

    feat(exercise): generate pythagorean triples

--- src/exercises/Pythagoras.tsx
+++ src/exercises/Pythagoras.tsx
@@ -1,4 +1,5 @@
 import { cache } from '@solidjs/router'
+import { random, sample } from 'lodash-es'
 import { z } from 'zod'
 import ExerciseBase, { type ExerciseProps } from '~/components/ExerciseBase'
 import Math from '~/components/Math'
@@ -12,12 +13,44 @@ export const schema = z.object({
   c: z.string().optional(),
   unknown: z.literal('a').or(z.literal('b')).or(z.literal('c')),
   attempt: z.string().optional(),
+  width: z.number().optional(),
+  height: z.number().optional(),
 })
 type State = z.infer<typeof schema>
 
+type Params = {
+  type: 'integers'
+  N: number
+  K: number
+  X: string[]
+}
+
+export function generate(params: Params): State {
+  const m = random(1, params.N)
+  const n = random(1, params.N)
+  const k = random(1, params.K)
+  const x = sample(params.X)!
+  let a = k * (m ** 2 - n ** 2)
+  let b = 2 * k * m * n
+  let c = k * (m ** 2 + n ** 2)
+  const width = 400
+  const height = (width * b) / a
+  const unknown = sample(['a', 'b', 'c']) as 'a' | 'b' | 'c'
+  const state = {
+    a: String(a),
+    b: String(b),
+    c: String(c),
+    height,
+    width,
+    unknown,
+  }
+  state[unknown] = x
+  return state
+}
+
 export const mark = cache(async (state: State) => {
   'use server'
-  const data = {...state}
+  const data = { ...state }
   data[state.unknown] = state.attempt
   const expr = `(${data.a})^2 + (${data.b})^2 - (${data.c})^2`
   const { expression } = await request(
@@ -34,9 +67,9 @@ export const mark = cache(async (state: State) => {
   return expression.isEqual
 }, 'checkPythagoras')
 
-export default function Pythagoras(props: ExerciseProps<State, null>) {
+export default function Pythagoras(props: ExerciseProps<State, Params>) {
   return (
-    <ExerciseBase type="Pythagoras" {...props} schema={schema} mark={mark}>
+    <ExerciseBase type="Pythagoras" {...props} schema={schema} mark={mark} generate={generate}>
       <p>
         Trouvez la valeur de <Math value={props.state?.[props.state?.unknown]} />
       </p>
@@ -44,8 +77,8 @@ export default function Pythagoras(props: ExerciseProps<State, null>) {
         a={props.state?.a}
         b={props.state?.b}
         c={props.state?.c}
-        height={400}
-        width={400}
+        height={props.state?.height || 400}
+        width={props.state?.width || 400}
       />
       <div class="flex items-center gap-2">
         <Math value={`${props.state?.[props.state?.unknown]}=`} />
