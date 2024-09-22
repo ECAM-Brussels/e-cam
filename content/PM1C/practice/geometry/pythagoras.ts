05217c03ec0b407fccc59694e5ae3e1710e8aaae
Khoi Nguyen <khoi@nguyen.me.uk>
2024-09-22 13:12:47 +0200

    content(pm1c): add pythagoras

---
+++ content/PM1C/practice/geometry/pythagoras.ts
@@ -0,0 +1,15 @@
+;({
+  mode: 'dynamic',
+  whiteboard: true,
+  data: [
+    {
+      type: 'Pythagoras',
+      params: {
+        type: 'integers',
+        N: 5,
+        K: 3,
+        X: ['x'],
+      },
+    },
+  ],
+}) satisfies import('~/components/ExerciseSequence').ExerciseProps
