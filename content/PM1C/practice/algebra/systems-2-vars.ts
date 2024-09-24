167bf02f3e0306b8b95ae083cd71429188d078b1
Khoi Nguyen <khoi@nguyen.me.uk>
2024-09-24 20:54:40 +0200

    fix(exercise): smaller numbers for Gauss

--- content/PM1C/practice/algebra/systems-2-vars.ts
+++ content/PM1C/practice/algebra/systems-2-vars.ts
@@ -8,7 +8,7 @@
       params: {
         variables: ['x', 'y'],
         L: [...Array.from({ length: 11 }, (_, i) => String(i - 5))],
-        U: [...Array.from({ length: 21 }, (_, i) => String(i - 10))],
+        U: [...Array.from({ length: 11 }, (_, i) => String(i - 5))],
         X: [...Array.from({ length: 21 }, (_, i) => String(i - 10))],
       },
     },
