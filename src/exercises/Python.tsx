23eb740aa8427152475f459b8da5713b52650326
Khoi Nguyen <khoi@nguyen.me.uk>
2024-09-27 16:37:07 +0200

    doc(exercise): python

--- src/exercises/Python.tsx
+++ src/exercises/Python.tsx
@@ -8,11 +8,45 @@ import { decrypt } from '~/lib/cryptography'
 import { wrapCode } from '~/lib/helpers'
 
 export const schema = z.object({
+  /**
+   * Question shown to the student, in markdown.
+   * This string will be dedented.
+   */
   question: z.string(),
+  /**
+   * (Optional) Will contain the student's attempt.
+   * If a value is supplied,
+   * this will prefill the text editor for the student with the desired value.
+   */
   code: z.string().optional(),
+  /**
+   * Code that should give the correct solution.
+   * It does not need to satisfy the same constraints as the student's code.
+   * 
+   * As an example, if students need to reimplement the factorial function without importing 'math',
+   * this code will still be allowed to import it.
+   */
   answer: z.string(),
+  /**
+   * List of python instructions to run on the student's code (see `code`)
+   * and on the model example (see `answer`).
+   * An exercise is considered correct if both codes give the same output for every instruction.
+   */
   tests: z.string().array(),
+  /**
+   * Whether to wrap the student's code in a function (called `main`) for testing.
+   * The 'main' function takes an array of strings as argument.
+   * These strings are used to mock the input() function
+   */
   wrap: z.boolean().optional(),
+  /**
+   * A list of constraints that the student's code must satisfy to be correct.
+   * A constraint is a tuple, with a regex given as a string, and a result given as a boolean.
+   * 
+   * @example
+   * ['import\\s+math', false]] // the student's code cannot contain 'import\\s+math'
+   * ['import\\s+math', true]] // the student's code must contain 'import\\s+math'
+   */
   constraints: z.tuple([z.string(), z.boolean()]).array().optional(),
 })
 export type State = z.infer<typeof schema>
