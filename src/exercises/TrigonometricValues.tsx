9b4b674614c59aba68e73435a8807e20522c4964
Khoi Nguyen <khoi@nguyen.me.uk>
2024-10-10 18:26:54 +0200

    feat(exercise): add solver for trig values

--- src/exercises/TrigonometricValues.tsx
+++ src/exercises/TrigonometricValues.tsx
@@ -62,6 +62,24 @@ export const mark = cache(async (state: State) => {
   return attempt.isEqual && attempt.isNumber
 }, 'checkTrigonometricValue')
 
+export const solve = cache(async (state: State) => {
+  'use server'
+
+  const { expression } = await request(
+    graphql(`
+      query SolveTrigonometricValue($expr: Math!) {
+        expression(expr: $expr) {
+          simplify {
+            expr
+          }
+        }
+      }
+    `),
+    state,
+  )
+  return { ...state, attempt: expression.simplify.expr }
+}, 'checkTrigonometricValue')
+
 export default function TrigonometricValues(
   props: ExerciseProps<State, Parameters<typeof generate>[0]>,
 ) {
@@ -72,11 +90,17 @@ export default function TrigonometricValues(
       schema={schema}
       mark={mark}
       generate={generate}
+      solve={solve}
+      solution={
+        <p>
+          La réponse est <Math value={props.feedback?.solution?.attempt} />
+        </p>
+      }
     >
       <Table />
       <p>
-        Sans utiliser la calculatrice mais en employant le tableau ci-dessus,
-        donnez la valeur exacte du nombre trigonométrique suivant.
+        Sans utiliser la calculatrice mais en employant le tableau ci-dessus, donnez la valeur
+        exacte du nombre trigonométrique suivant.
       </p>
       <div class="flex items-center gap-2">
         <Math value={`${props.state?.expr}=`} displayMode />
@@ -95,42 +119,90 @@ export default function TrigonometricValues(
 function Table() {
   const functions = ['sin', 'cos', 'tan']
   const angles = ''
-  return <table class="border container text-center mb-8">
-    <thead>
-      <tr class="border-b">
-        <th class="border-e"></th>
-        <th><Math value="0" /></th>
-        <th><Math value="\frac {\pi} 6" /></th>
-        <th><Math value="\frac {\pi} 4" /></th>
-        <th><Math value="\frac {\pi} 3" /></th>
-        <th><Math value="\frac {\pi} 2" /></th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <th class="border-r"><Math value="\sin" /></th>
-        <td><Math value="0" /></td>
-        <td><Math value="\frac 1 2" /></td>
-        <td><Math value="\frac {\sqrt 2} 2" /></td>
-        <td><Math value="\frac {\sqrt 3} 2" /></td>
-        <td><Math value="1" /></td>
-      </tr>
-      <tr>
-        <th class="border-r"><Math value="\cos" /></th>
-        <td><Math value="1" /></td>
-        <td><Math value="\frac {\sqrt 3} 2" /></td>
-        <td><Math value="\frac {\sqrt 2} 2" /></td>
-        <td><Math value="\frac 1 2" /></td>
-        <td><Math value="0" /></td>
-      </tr>
-      <tr>
-        <th class="border-r"><Math value="\tan" /></th>
-        <td><Math value="0" /></td>
-        <td><Math value="\frac {\sqrt 3} 3" /></td>
-        <td><Math value="1" /></td>
-        <td><Math value="\sqrt 3" /></td>
-        <td><Math value="\nexists" /></td>
-      </tr>
-    </tbody>
-  </table>
+  return (
+    <table class="border container text-center mb-8">
+      <thead>
+        <tr class="border-b">
+          <th class="border-e"></th>
+          <th>
+            <Math value="0" />
+          </th>
+          <th>
+            <Math value="\frac {\pi} 6" />
+          </th>
+          <th>
+            <Math value="\frac {\pi} 4" />
+          </th>
+          <th>
+            <Math value="\frac {\pi} 3" />
+          </th>
+          <th>
+            <Math value="\frac {\pi} 2" />
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th class="border-r">
+            <Math value="\sin" />
+          </th>
+          <td>
+            <Math value="0" />
+          </td>
+          <td>
+            <Math value="\frac 1 2" />
+          </td>
+          <td>
+            <Math value="\frac {\sqrt 2} 2" />
+          </td>
+          <td>
+            <Math value="\frac {\sqrt 3} 2" />
+          </td>
+          <td>
+            <Math value="1" />
+          </td>
+        </tr>
+        <tr>
+          <th class="border-r">
+            <Math value="\cos" />
+          </th>
+          <td>
+            <Math value="1" />
+          </td>
+          <td>
+            <Math value="\frac {\sqrt 3} 2" />
+          </td>
+          <td>
+            <Math value="\frac {\sqrt 2} 2" />
+          </td>
+          <td>
+            <Math value="\frac 1 2" />
+          </td>
+          <td>
+            <Math value="0" />
+          </td>
+        </tr>
+        <tr>
+          <th class="border-r">
+            <Math value="\tan" />
+          </th>
+          <td>
+            <Math value="0" />
+          </td>
+          <td>
+            <Math value="\frac {\sqrt 3} 3" />
+          </td>
+          <td>
+            <Math value="1" />
+          </td>
+          <td>
+            <Math value="\sqrt 3" />
+          </td>
+          <td>
+            <Math value="\nexists" />
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  )
 }
