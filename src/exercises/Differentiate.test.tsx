85aef7999934dfb4253f1d40918f88ab64852f72
Khoi Nguyen <khoi@nguyen.me.uk>
2024-10-11 12:48:12 +0200

    test(exercise): add tests for derivative

--- src/exercises/Differentiate.test.tsx
+++ src/exercises/Differentiate.test.tsx
@@ -6,4 +6,6 @@ test('marks correctly', async () => {
   expect(await mark({ expr: '\\sin(x)', attempt: '\\cos(x)' })).toBe(true)
   expect(await mark({ expr: 'e^x', attempt: 'e^x' })).toBe(true)
   expect(await mark({ expr: 'x(x - 1)', attempt: '2x - 1' })).toBe(true)
+  expect(await mark({ expr: '\\frac{x^2}{\\ln x}', attempt: '\\frac{2x}{\\ln x} - \\frac{x}{\\ln{(x)^2}}'})).toBe(false)
+  expect(await mark({ expr: '\\frac{x^2}{\\ln x}', attempt: '\\frac{2x}{\\ln x} - \\frac{x}{\\ln^2(x)}'})).toBe(true)
 })
