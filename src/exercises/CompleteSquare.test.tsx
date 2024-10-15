e04dabbd3dc52884e0fd5d0ffb8632da088fa07d
Khoi Nguyen <khoi@nguyen.me.uk>
2024-10-15 11:53:34 +0200

    test(exercise): add test for complete the square

--- src/exercises/CompleteSquare.test.tsx
+++ src/exercises/CompleteSquare.test.tsx
@@ -3,4 +3,5 @@ import { test, expect } from 'vitest'
 
 test('marks correctly', async () => {
   expect(await mark({ expr: 'x^2 + 10x + 24', attempt: '(x + 5)^2 -1' })).toBe(true)
+  expect(await mark({ expr: 'x^2 + 2x - 2', attempt: '(x + 1)^2 - 3' })).toBe(true)
 })
