986d2cb11ced56c548a74e4dfa0ea642c7ea5087
Khoi Nguyen <khoi@nguyen.me.uk>
2025-01-15 15:27:26 +0100

    feat(code): add support for solidjs

--- src/components/Javascript.tsx
+++ src/components/Javascript.tsx
@@ -1,8 +1,10 @@
 import dedent from 'dedent-js'
+import { createResource } from 'solid-js'
 import Html, { type Props } from '~/components/Html'
+import { transform } from '~/lib/repl/babel'
 
 type JavascriptProps = Props & {
-  framework?: 'react'
+  framework?: 'react' | 'solid'
 }
 
 function fixCode(code: string): string {
@@ -24,7 +26,7 @@ function fixCode(code: string): string {
 }
 
 export default function Javascript(props: JavascriptProps) {
-  const code = () => {
+  const [code] = createResource(() => props.value, async (value: string): Promise<string> => {
     if (props.framework === 'react') {
       return dedent`
         <script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
@@ -33,17 +35,27 @@ export default function Javascript(props: JavascriptProps) {
         <script type="text/babel" data-presets="react" data-type="module">
         import React, { useState, useEffect, useMemo } from 'https://esm.sh/react@18';
         import ReactDOM from 'https://esm.sh/react-dom@18';
-        ${fixCode(props.value)}
+        ${fixCode(value)}
         const root = ReactDOM.createRoot(document.getElementById('app'));
         root.render(React.createElement(App, null));
+        </script>
+      `
+    } else if (props.framework === 'solid') {
+      value = (await transform(value, { presets: ['babel-preset-solid'] })) || ''
+      return dedent`
+        <script type="module">
+        import { createSignal } from "https://esm.sh/solid-js"
+        ${value}
+        import { render } from "https://esm.sh/solid-js/web";
+        render(App, document.body)
         </script>
       `
     }
     return dedent`
       <script>
-      ${fixCode(props.value)}
+      ${fixCode(value)}
       </script>
     `
-  }
-  return <Html {...props} value={code()} />
+  })
+  return <Html {...props} value={code() || ''} />
 }
---
+++ src/lib/repl/babel.ts
@@ -0,0 +1,55 @@
+import type * as Babel from '@babel/standalone'
+
+type Options = {
+  framework?: 'react' | 'solidjs'
+  babel?: Promise<typeof Babel>
+  presets?: string[]
+}
+
+const cdn = 'https://esm.sh'
+
+function importPlugin() {
+  function transformImportee(importee: string) {
+    return `https://esm.sh/${importee}`
+  }
+  return {
+    visitor: {
+      Import(path: any) {
+        const importee: string = path.parent.arguments[0].value
+        path.parent.arguments[0].value = transformImportee(importee)
+      },
+      ImportDeclaration(path: any) {
+        const importee: string = path.node.source.value
+        path.node.source.value = transformImportee(importee)
+      },
+      ExportAllDeclaration(path: any) {
+        const importee: string = path.node.source.value
+        path.node.source.value = transformImportee(importee)
+      },
+      ExportNamedDeclaration(path: any) {
+        const importee: string = path.node.source?.value
+        if (importee) {
+          path.node.source.value = transformImportee(importee)
+        }
+      },
+    },
+  }
+}
+
+export async function transform(code: string, options: Options = {}) {
+  const babel = await (options.babel ||
+    (import(/* @vite-ignore */ `${cdn}/@babel/standalone`) as Promise<typeof Babel>))
+
+  const presets = await Promise.all(
+    (options.presets || []).map(async (name) => {
+      if (name in babel.availablePresets) {
+        return babel.availablePresets[name]
+      } else {
+        return await import(/* @vite-ignore */ `${cdn}/${name}`).then((m) => m.default)
+      }
+    }),
+  )
+
+  code = babel.transform(code, { presets, plugins: [importPlugin] }).code || ''
+  return code
+}
