d15d6a13803d0148f2cd79eef3e2a61fdfb58c2f
Khoi Nguyen <khoi@nguyen.me.uk>
2025-01-19 12:05:19 +0100

    chore(js): refactor transpilation

--- src/components/Javascript.tsx
+++ src/components/Javascript.tsx
@@ -1,72 +1,21 @@
 import dedent from 'dedent-js'
 import { createResource } from 'solid-js'
 import Html, { type Props } from '~/components/Html'
-import { svelteTransform, transform } from '~/lib/repl/babel'
+import { transform } from '~/lib/repl/babel'
 
 type JavascriptProps = Props & {
   framework?: 'react' | 'solid' | 'svelte'
 }
 
-function fixCode(code: string): string {
-  code = code.replace(
-    /(import\s+(\w+,?\s*)?(\{[^}]*\})?\s*(from\s*)?)['"](.+)['"]\s*;?/gm,
-    (_, begin, __, ___, ____, packageName) => `${begin}'${packageName}';`,
-  )
-  const prefix = dedent`
-    const _logs = []
-    const _node = document.createElement('pre')
-    document.body.appendChild(_node)
-    console.log = function (...args) {
-      _logs.push(args.join(' '))
-      _node.innerText = _logs.join('\\n')
-    }
-
-  `
-  return prefix + code
-}
-
 export default function Javascript(props: JavascriptProps) {
-  const [code] = createResource(() => props.value, async (value: string): Promise<string> => {
-    if (props.framework === 'react') {
-      return dedent`
-        <script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
-        <div id="app">
-        </div>
-        <script type="text/babel" data-presets="react" data-type="module">
-        import React, { useState, useEffect, useMemo } from 'https://esm.sh/react@18';
-        import ReactDOM from 'https://esm.sh/react-dom@18';
-        ${fixCode(value)}
-        const root = ReactDOM.createRoot(document.getElementById('app'));
-        root.render(React.createElement(App, null));
-        </script>
-      `
-    } else if (props.framework === 'solid') {
-      value = (await transform(value, { presets: ['babel-preset-solid'] })) || ''
-      return dedent`
+  const [code] = createResource(
+    () => props.value,
+    async (value: string): Promise<string> =>
+      dedent`
         <script type="module">
-        import { createSignal } from "https://esm.sh/solid-js"
-        ${value}
-        import { render } from "https://esm.sh/solid-js/web";
-        render(App, document.body)
+        ${await transform(value, props.framework)}
         </script>
-      `
-    } else if (props.framework === 'svelte') {
-      value = await svelteTransform(value)
-      return dedent`
-        <div id="app">
-        </div>
-        <script type="module">
-        ${value}
-        import { mount } from 'https://esm.sh/svelte'
-        mount(Component, { target: document.getElementById('app') })
-        </script>
-      `
-    }
-    return dedent`
-      <script>
-      ${fixCode(value)}
-      </script>
-    `
-  })
+      `,
+  )
   return <Html {...props} value={code() || ''} />
 }
--- src/lib/repl/babel.ts
+++ src/lib/repl/babel.ts
@@ -1,12 +1,6 @@
 import type * as Babel from '@babel/standalone'
 import dedent from 'dedent-js'
 
-type Options = {
-  framework?: 'react' | 'solidjs' | 'svelte'
-  babel?: Promise<typeof Babel>
-  presets?: string[]
-}
-
 const cdn = 'https://esm.sh'
 
 function importPlugin() {
@@ -37,12 +31,10 @@ function importPlugin() {
   }
 }
 
-export async function transform(code: string, options: Options = {}) {
-  const babel = await (options.babel ||
-    (import(/* @vite-ignore */ `${cdn}/@babel/standalone`) as Promise<typeof Babel>))
-
+async function babelTransform(code: string, presetNames: string[] = []) {
+  const babel = (await import(/* @vite-ignore */ `${cdn}/@babel/standalone`)) as typeof Babel
   const presets = await Promise.all(
-    (options.presets || []).map(async (name) => {
+    presetNames.map(async (name) => {
       if (name in babel.availablePresets) {
         return babel.availablePresets[name]
       } else {
@@ -50,14 +42,55 @@ export async function transform(code: string, options: Options = {}) {
       }
     }),
   )
-
-  code = babel.transform(code, { presets, plugins: [importPlugin] }).code || ''
-  return code
+  return babel.transform(code, { presets, plugins: [importPlugin] }).code || ''
 }
 
-export async function svelteTransform(code: string) {
-  const compiler = await import(/* @vite-ignore */`${cdn}/svelte/compiler`)
-  code = compiler.compile(code, { name: 'Component' }).js.code
-  code = await transform(code, {})
-  return code
+export async function transform(code: string, framework?: 'react' | 'solid' | 'svelte') {
+  let presets: string[] = []
+  let before: string = ''
+  let after: string = ''
+  if (framework === 'react') {
+    presets = ['react']
+    before = dedent`
+      import React, { useEffect, useState } from "https://esm.sh/react"
+    `
+    after = dedent`
+      import { createRoot } from 'https://esm.sh/react-dom/client';
+      const root = createRoot(document.body);
+      root.render(React.createElement(App, null));
+    `
+  } else if (framework === 'solid') {
+    presets = ['babel-preset-solid']
+    before = dedent`
+      import { createEffect, createSignal } from "https://esm.sh/solid-js"
+    `
+    after = dedent`
+      if (App) {
+        import { render } from "https://esm.sh/solid-js/web";
+        render(App, document.body)
+      }
+    `
+  } else if(framework === 'svelte') {
+    const compiler = await import(/* @vite-ignore */ `${cdn}/svelte/compiler`)
+    code = compiler.compile(code, { name: 'Component' }).js.code
+    presets = []
+    after = dedent`
+      import { mount } from 'https://esm.sh/svelte'
+      mount(Component, { target: document.body })
+    `
+  }
+  code = await babelTransform(code, presets)
+
+  return dedent`
+    const _logs = []
+    const _node = document.createElement('pre')
+    document.body.appendChild(_node)
+    console.log = function (...args) {
+      _logs.push(args.join(' '))
+      _node.innerText = _logs.join('\\n')
+    }
+    ${before}
+    ${code}
+    ${after}
+  `
 }
