29cb75b3000ba0345be1bd0c3b94e06c30109fcc
Khoi Nguyen <khoi@nguyen.me.uk>
2025-02-21 12:07:08 +0100

    feat(page): add error boundary

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,4 +1,5 @@
 import MetaProvider, { type MetaProviderProps } from './MetaProvider'
+import { ErrorBoundary } from 'solid-js'
 import Breadcrumbs from '~/components/Breadcrumbs'
 import Navbar from '~/components/Navbar'
 
@@ -9,7 +10,18 @@ export default function Page(props: PageProps) {
     <MetaProvider title={props.title} lang={props.lang}>
       <Navbar />
       <Breadcrumbs />
-      <div class="container mx-auto p-4">{props.children}</div>
+      <div class="container mx-auto p-4">
+        <ErrorBoundary
+          fallback={(error) => (
+            <div class="rounded-xl border bg-white p-8 shadow">
+              <h1 class="font-bold text-3xl mb-8">Something went wrong</h1>
+              <p>{error.toString()}</p>
+            </div>
+          )}
+        >
+          {props.children}
+        </ErrorBoundary>
+      </div>
     </MetaProvider>
   )
 }
