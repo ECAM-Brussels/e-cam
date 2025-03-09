191f8483c838aaa3338dd3953f0f5edcc2a6c7cb
Khoi Nguyen <khoi@nguyen.me.uk>
2025-03-09 22:22:49 +0100

    chore(prism): add name to textarea

--- src/components/PrismEditor.tsx
+++ src/components/PrismEditor.tsx
@@ -1,3 +1,4 @@
+import { createEffect } from 'solid-js'
 import { Editor } from 'solid-prism-editor'
 import 'solid-prism-editor/layout.css'
 import 'solid-prism-editor/prism/languages/common'
@@ -5,6 +6,21 @@ import 'solid-prism-editor/search.css'
 import { basicSetup } from 'solid-prism-editor/setups'
 import 'solid-prism-editor/themes/github-light.css'
 
-export default function PrismEditor(props: Parameters<typeof Editor>[0]) {
-  return <Editor extensions={basicSetup} tabSize={props.language === 'python' ? 4 : 2} {...props} />
+export default function PrismEditor(props: Parameters<typeof Editor>[0] & { name: string }) {
+  let textarea: HTMLTextAreaElement
+  createEffect(() => {
+    if (textarea && props.name) {
+      textarea.name = props.name
+    }
+  })
+  return (
+    <Editor
+      extensions={basicSetup}
+      tabSize={props.language === 'python' ? 4 : 2}
+      {...props}
+      onMount={(editor) => {
+        textarea = editor.textarea
+      }}
+    />
+  )
 }
