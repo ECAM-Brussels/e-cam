1ee09edf56de36d3e7d7c3dc35996eef645e1818
Khoi Nguyen <khoi@nguyen.me.uk>
2025-03-17 13:05:52 +0100

    chore(assignment): add pagination support

--- src/components/Assignment.tsx
+++ src/components/Assignment.tsx
@@ -1,7 +1,8 @@
 import ErrorBoundary from './ErrorBoundary'
-import { createAsync, revalidate } from '@solidjs/router'
-import { type Component } from 'solid-js'
-import { Dynamic, For } from 'solid-js/web'
+import Pagination from './Pagination'
+import { createAsync } from '@solidjs/router'
+import { Show, type Component } from 'solid-js'
+import { Dynamic } from 'solid-js/web'
 import {
   type AssignmentProps,
   exercises,
@@ -11,21 +12,43 @@ import {
 } from '~/lib/exercises/assignment'
 import { ExerciseComponentProps } from '~/lib/exercises/base'
 
-export default function Assignment(props: AssignmentProps) {
+export default function Assignment(
+  props: AssignmentProps & { index: number; onIndexChange: (newIndex: number) => void },
+) {
   const primary = () => ({ url: props.url, userEmail: props.userEmail, id: props.id })
   const key = () => [primary(), props.mode, props.streak, props.body] as const
   const body = createAsync(async () => getAssignmentBody(...key()))
+  const classes = () =>
+    body()?.map((exercise: Exercise) => {
+      if (exercise.feedback?.correct) {
+        return 'bg-green-100'
+      } else if (exercise.feedback?.correct === false) {
+        return 'bg-red-100'
+      }
+      return 'bg-white'
+    })
   return (
     <ErrorBoundary>
-      <For each={body()}>
-        {function <State, P, Sol>(exercise: Exercise, pos: () => number) {
+      <Show when={props.title}>
+        <h1 class="text-4xl my-4">{props.title}</h1>
+      </Show>
+      <Show when={body()}>
+        <Pagination
+          current={props.index}
+          onChange={props.onIndexChange}
+          max={body()?.length || 0}
+          classes={classes()}
+        />
+      </Show>
+      <Show when={body()?.[props.index]} keyed>
+        {function <State, P, Sol>(exercise: Exercise) {
           const exerciseProps = exercise as ExerciseComponentProps<State, P, Sol>
           const component = exercises[exercise.type] as Component<typeof exerciseProps>
           async function save(event: {
             state: State
             feedback?: { correct: boolean; solution: Sol }
           }) {
-            await saveExercise(primary(), pos(), {
+            await saveExercise(primary(), props.index, {
               type: exercise.type,
               feedback: event.feedback,
               state: event.state,
@@ -35,7 +58,7 @@ export default function Assignment(props: AssignmentProps) {
             <Dynamic component={component} {...exerciseProps} onGenerate={save} onSubmit={save} />
           )
         }}
-      </For>
+      </Show>
     </ErrorBoundary>
   )
 }
