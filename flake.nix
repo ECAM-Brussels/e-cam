79ae5e8eb82ae4cc7f9e5f347c64ea0dc6d9a5a9
Khoi Nguyen <khoi@nguyen.me.uk>
2025-04-11 09:12:21 +0200

    feat(nixos): coordinate prisma versions

--- flake.nix
+++ flake.nix
@@ -13,8 +13,10 @@
           nodePackages.prisma
           nodePackages.npm
           nodejs-slim
+          openssl
         ];
         shellHook = with pkgs; ''
+          export OPENSSL_DIR=${pkgs.openssl.dev}
           export PRISMA_SCHEMA_ENGINE_BINARY="${prisma-engines}/bin/schema-engine"
           export PRISMA_QUERY_ENGINE_BINARY="${prisma-engines}/bin/query-engine"
           export PRISMA_QUERY_ENGINE_LIBRARY="${prisma-engines}/lib/libquery_engine.node"
