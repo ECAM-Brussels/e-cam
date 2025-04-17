aae7d3264bea8d1373a7e7d36e45955d5277f356
Khoi Nguyen <khoi@nguyen.me.uk>
2025-04-17 12:31:51 +0200

    fix(assignment): remove old code

--- src/exercises/Limit.test.tsx
+++
@@ -1,8 +0,0 @@
-import { mark } from './Limit'
-import { test, expect } from 'vitest'
-
-test('marks correctly', async () => {
-  expect(await mark({ expr: '\\frac{x}{x}', x0: '0', attempt: '0' })).toBe(false)
-  expect(await mark({ expr: '\\frac{x}{x}', x0: '0', attempt: '1' })).toBe(true)
-  expect(await mark({ expr: '\\frac{x^2 - 5x + 6}{x - 2}', x0: '2', attempt: '-1' })).toBe(true)
-})
--- src/vite/pandoc.ts
+++ src/vite/pandoc.ts
@@ -1,42 +1,11 @@
-import { type ExerciseProps } from '../components/ExerciseSequence'
-import { encrypt } from '../lib/cryptography'
-import { wrapCode } from '../lib/helpers'
-import { PrismaClient } from '@prisma/client'
-import { exec as execWithCallback, spawn } from 'child_process'
-import dedent from 'dedent-js'
+import { exec as execWithCallback } from 'child_process'
 import glob from 'fast-glob'
-import { mkdirSync, readFileSync, writeFileSync } from 'fs'
+import { mkdirSync, readFileSync } from 'fs'
 import { dirname, relative, resolve } from 'path'
-import { transpile } from 'typescript'
 import { promisify } from 'util'
-import { type Plugin, loadEnv } from 'vite'
+import { type Plugin } from 'vite'
 
 const exec = promisify(execWithCallback)
-
-function python(code: string) {
-  return new Promise((resolve, reject) => {
-    const process = spawn('python', ['-c', code])
-
-    let stdout = ''
-    let stderr = ''
-
-    process.stdout.on('data', (data) => {
-      stdout += data.toString()
-    })
-
-    process.stderr.on('data', (data) => {
-      stderr += data.toString()
-    })
-
-    process.on('close', (code) => {
-      if (code !== 0) {
-        reject(new Error(`Process exited with code ${code}: ${stderr}`))
-      } else {
-        resolve(stdout.trim())
-      }
-    })
-  })
-}
 
 async function generatePage(file: string) {
   const relativePath = relative(resolve('content'), file)
@@ -77,39 +46,6 @@ async function generatePage(file: string) {
   }
 }
 
-async function createAssignment(file: string, passphrase: string, prisma: PrismaClient) {
-  const relativePath = relative(resolve('content'), file)
-  const outputPath = resolve('src/routes/(generated)', relativePath.replace(/\.ts$/, '.tsx'))
-  mkdirSync(dirname(outputPath), { recursive: true })
-  const contents = String(readFileSync(file, 'utf-8'))
-  const assignment = eval(transpile(contents)) as ExerciseProps
-  const url = file.replace('/index.ts', '').replace('.ts', '').replace('content', '')
-  const title = assignment.title || ''
-  try {
-    await prisma.page.upsert({ where: { url }, update: { title }, create: { url, title } })
-  } catch {
-    console.log('Upsert failed')
-  }
-  for (const exercise of assignment.data) {
-    if (exercise.type === 'Simple' && exercise.state) {
-      exercise.state.answer = encrypt(exercise.state.answer, passphrase)
-      exercise.state.question = dedent(exercise.state.question)
-    } else if (exercise.type === 'Python' && exercise.state) {
-      let code = dedent(exercise.state.answer)
-      if (exercise.state.wrap) {
-        code = wrapCode(code)
-      }
-      let results = await Promise.all(
-        exercise.state.tests.map((test) => python(`${code}\n\nprint(${test})`)),
-      )
-      exercise.state.answer = encrypt(JSON.stringify(results), passphrase)
-    }
-  }
-  const template = String(readFileSync(resolve('src/vite/assignment.tsx'), 'utf-8'))
-  const content = template.replace('$body$', JSON.stringify(assignment, null, 2))
-  writeFileSync(outputPath, content, 'utf-8')
-}
-
 async function generateImports() {
   const components = await glob.glob('src/components/*.tsx')
   let imports = ''
@@ -130,40 +66,20 @@ async function generateImports() {
   return imports
 }
 
-async function buildAll(passphrase: string, prisma: PrismaClient) {
+async function buildAll() {
   const pages = await glob.glob('content/**/*.md')
   for (const file of pages) {
     generatePage(file)
   }
-  const assignments = await glob.glob('content/**/*.ts')
-  for (const file of assignments) {
-    createAssignment(file, passphrase, prisma)
-  }
 }
 
 const pandocPlugin = (): Plugin => {
-  let passphrase = ''
-  let prisma: PrismaClient
   return {
     name: 'pandoc-plugin',
     buildStart() {
-      buildAll(passphrase, prisma)
-    },
-    configResolved(config) {
-      const env = loadEnv(config.mode, process.cwd(), 'VITE')
-      passphrase = env.VITE_PASSPHRASE
-      prisma = new PrismaClient({
-        datasources: {
-          db: {
-            url: env.VITE_DATABASE_URL,
-          },
-        },
-      })
+      buildAll()
     },
     async handleHotUpdate({ file }) {
-      if (file.endsWith('.ts') && file.startsWith(resolve('content'))) {
-        createAssignment(file, passphrase, prisma)
-      }
       if (file.endsWith('.md') && file.startsWith(resolve('content'))) {
         generatePage(file)
       }
