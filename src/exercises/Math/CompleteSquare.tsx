dcbdfb89a4944548505ae8291010d93f97a24b48
Khoi Nguyen <khoi@nguyen.me.uk>
2025-04-28 16:29:41 +0200

    feat(CompleteSquare): allow multiline submissions

--- src/exercises/Math/CompleteSquare.tsx
+++ src/exercises/Math/CompleteSquare.tsx
@@ -1,5 +1,10 @@
+import { faPlus, faTrash } from '@fortawesome/free-solid-svg-icons'
 import { sample } from 'lodash-es'
+import { onMount, Show } from 'solid-js'
+import { For } from 'solid-js'
+import { createStore } from 'solid-js/store'
 import { z } from 'zod'
+import Fa from '~/components/Fa'
 import Math from '~/components/Math'
 import { graphql } from '~/gql'
 import { createExerciseType } from '~/lib/exercises/base'
@@ -7,18 +12,46 @@ import { request } from '~/lib/graphql'
 
 const { Component, schema } = createExerciseType({
   name: 'CompleteSquare',
-  Component: (props) => (
-    <>
-      <p class="my-4">Complétez le carré dans l'expression suivante</p>
-      <div class="flex justify-center items-center gap-2">
-        <Math value={`${props.expr} =`} />
-        <Math class="border min-w-24 p-2" editable name="attempt" value={props.attempt} />
-      </div>
-    </>
-  ),
+  Component: (props) => {
+    const [lines, setLines] = createStore(props.attempt || [''])
+    onMount(() => {})
+    return (
+      <>
+        <p class="my-4">Complétez le carré dans l'expression suivante.</p>
+        <For each={lines}>
+          {(line, i) => (
+            <div class="grid grid-cols-2 justify-center items-center gap-2 my-1">
+              <Math class="justify-self-end" value={`${i() == 0 ? props.expr : ''} =`} />
+              <div class="flex gap-2">
+                <Math class="border min-w-24 p-2" editable name="attempt" value={line} />
+                <button type="button" onClick={() => setLines(lines.length, '')}>
+                  <Fa icon={faPlus} />
+                </button>
+                <Show when={i() > 0}>
+                  <button
+                    type="button"
+                    onClick={() => setLines(lines.filter((_, index) => index !== i()))}
+                  >
+                    <Fa icon={faTrash} />
+                  </button>
+                </Show>
+              </div>
+            </div>
+          )}
+        </For>
+      </>
+    )
+  },
   state: z.object({
     expr: z.string(),
-    attempt: z.undefined().or(z.string().min(1)),
+    attempt: z.union([
+      z.undefined(),
+      z
+        .string()
+        .min(1)
+        .transform((s) => [s]),
+      z.string().min(1).array(),
+    ]),
   }),
   mark: async (state) => {
     const { attempt } = await request(
@@ -30,7 +63,7 @@ const { Component, schema } = createExerciseType({
           }
         }
       `),
-      { attempt: '', ...state },
+      { ...state, attempt: state.attempt?.at(-1) ?? '' },
     )
     return attempt.isEqual && attempt.count == 1
   },
