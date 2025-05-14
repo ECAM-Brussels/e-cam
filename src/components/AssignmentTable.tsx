f4f59f18b0d91bc4b1885375a50361ef73d0d14a
Khoi Nguyen <khoi@nguyen.me.uk>
2025-05-14 15:06:35 +0200

    feat(user): show basic assignment table

---
+++ src/components/AssignmentTable.tsx
@@ -0,0 +1,95 @@
+import Fa from './Fa'
+import { faChevronDown, faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import { createAsync, query } from '@solidjs/router'
+import { Show } from 'solid-js'
+import Table from '~/components/Table'
+import { getUser } from '~/lib/auth/session'
+import { prisma } from '~/lib/db'
+
+type Row = {
+  url: string
+  title: string
+  courses: { url: string; title: string; code: string }[]
+  prerequisites?: Row[]
+  grade: number
+}
+
+const getAssignmentTable = query(async (where: {}): Promise<Row[]> => {
+  'use server'
+  const user = await getUser()
+  const data = await prisma.assignment.findMany({
+    where,
+    select: {
+      url: true,
+      title: true,
+      courses: { select: { code: true, url: true, title: true } },
+      prerequisites: {
+        select: {
+          url: true,
+          title: true,
+          courses: { select: { code: true, url: true, title: true } },
+        },
+      },
+      attempts: user
+        ? {
+            select: { gain: true },
+            where: { email: user.email, gain: { not: 0 } },
+            orderBy: { position: 'desc' },
+            take: 10,
+          }
+        : false,
+    },
+  })
+  return data.map(({ attempts, ...info }) => {
+    return {
+      ...info,
+      grade: attempts.length || 0,
+      prerequisites: info.prerequisites.map((p) => ({
+        ...p,
+        grade: data.filter((r) => r.url === p.url)[0].attempts.length || 0,
+      })),
+    }
+  })
+}, 'getAssignmentTable')
+
+export default function AssignmentTable() {
+  const data = createAsync(() => getAssignmentTable({}), { initialValue: [] })
+  return (
+    <Table
+      data={data()}
+      subRows={(row) => row.prerequisites}
+      columns={[
+        {
+          id: 'expand',
+          cell: (info) => (
+            <Show when={info.row.original.prerequisites?.length}>
+              <button onClick={info.row.getToggleExpandedHandler()} title="Montrer les prérequis">
+                <Fa icon={info.row.getIsExpanded() ? faChevronRight : faChevronDown} />
+              </button>{' '}
+            </Show>
+          ),
+        },
+        {
+          accessorKey: 'title',
+          header: 'Title',
+          cell: (info) => (
+            <a href={info.row.original.url} style={{ 'margin-left': `${info.row.depth ?? 0}em` }}>
+              {info.getValue<string>()}
+            </a>
+          ),
+        },
+        {
+          accessorFn: (row) => row.courses.map((c) => c.title || c.code).join(', '),
+          header: 'Cours',
+        },
+        {
+          header: 'Progrès',
+          accessorFn: (row) => row.grade / 10,
+          cell: (info) => {
+            return <progress value={info.getValue<number>()} />
+          },
+        },
+      ]}
+    />
+  )
+}
--- src/routes/users/[email].tsx
+++ src/routes/users/[email].tsx
@@ -1,5 +1,6 @@
 import { createAsyncStore, useParams } from '@solidjs/router'
-import { Show } from 'solid-js'
+import { Suspense } from 'solid-js'
+import AssignmentTable from '~/components/AssignmentTable'
 import Graph from '~/components/Graph'
 import Page from '~/components/Page'
 import { getUserInfo } from '~/lib/user'
@@ -8,17 +9,16 @@ export default function () {
   const params = useParams()
   const user = createAsyncStore(() => getUserInfo(params.email))
   return (
-    <Show when={user()}>
-      {(user) => (
-        <Page title={`Profil de ${user().firstName} ${user().lastName}`}>
-          <section class="bg-white rounded-xl p-4 py-8 border">
-            <h1 class="font-bold text-3xl">
-              {user().lastName}, {user().firstName}
-            </h1>
-            <Graph class="min-h-96 w-full" />
-          </section>
-        </Page>
-      )}
-    </Show>
+    <Suspense>
+      <Page title={`Profil de ${user()?.firstName} ${user()?.lastName}`}>
+        <section class="bg-white rounded-xl p-4 py-8 border">
+          <h1 class="font-bold text-3xl">
+            {user()?.lastName}, {user()?.firstName}
+          </h1>
+          <AssignmentTable />
+          <Graph class="min-h-96 w-full" />
+        </section>
+      </Page>
+    </Suspense>
   )
 }
