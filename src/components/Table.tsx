d09de2b9f1f2e03fd5409ec20220995538cffd13
Khoi Nguyen <khoi@nguyen.me.uk>
2025-05-14 20:58:21 +0200

    feat(table): add filter

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -6,8 +6,10 @@ import {
   flexRender,
   getCoreRowModel,
   getExpandedRowModel,
+  getFilteredRowModel,
   getSortedRowModel,
 } from '@tanstack/solid-table'
+import { debounce } from 'lodash-es'
 import { createSignal, For, Show } from 'solid-js'
 import Fa from '~/components/Fa'
 
@@ -20,6 +22,8 @@ type TableProps<Row> = {
 
 export default function Table<Row extends object>(props: TableProps<Row>) {
   const [sorting, setSorting] = createSignal<SortingState>([])
+  const [globalFilter, setGlobalFilter] = createSignal('')
+  const debounceSetGlobalFilter = debounce((value: string) => setGlobalFilter(value), 50)
   const table = createSolidTable({
     get data() {
       return props.data
@@ -28,72 +32,88 @@ export default function Table<Row extends object>(props: TableProps<Row>) {
       get sorting() {
         return sorting()
       },
+      get globalFilter() {
+        return globalFilter()
+      },
     },
     columns: props.columns,
     getSubRows: props.subRows,
     onSortingChange: setSorting,
     getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onGlobalFilterChange: setGlobalFilter,
+    globalFilterFn: 'includesString',
     getSortedRowModel: getSortedRowModel(),
     getExpandedRowModel: getExpandedRowModel(),
   })
   return (
-    <table class={props.class ?? 'border bg-white text-gray-600 mx-auto my-4'}>
-      <thead class="border text-gray-900 uppercase bg-gray-50">
-        <For each={table.getHeaderGroups()}>
-          {(headerGroup) => (
-            <tr class="px-4 py-4 text-sm">
-              <For each={headerGroup.headers}>
-                {(header) => (
-                  <th
-                    class="px-4 py-2"
-                    classList={{
-                      'cursor-pointer select-none': header.column.getCanSort(),
-                    }}
-                    onClick={header.column.getToggleSortingHandler()}
-                  >
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(header.column.columnDef.header, header.getContext())}
-                    <Show
-                      when={header.column.getIsSorted()}
-                      fallback={
-                        <Show when={header.column.getCanSort()}>
-                          <Fa icon={faSort} class="mx-2 text-gray-400" />
-                        </Show>
-                      }
+    <div class="my-4">
+      <input
+        class="block p-2 font-lg shadow border border-block mx-auto my-8 w-96"
+        value={globalFilter() ?? ''}
+        onInput={(e) => debounceSetGlobalFilter(e.currentTarget.value)}
+        placeholder="Search all columns..."
+      />
+      <table class={props.class ?? 'border bg-white text-gray-600 mx-auto my-4'}>
+        <thead class="border text-gray-900 uppercase bg-gray-50">
+          <For each={table.getHeaderGroups()}>
+            {(headerGroup) => (
+              <tr class="px-4 py-4 text-sm">
+                <For each={headerGroup.headers}>
+                  {(header) => (
+                    <th
+                      class="px-4 py-2"
+                      classList={{
+                        'cursor-pointer select-none': header.column.getCanSort(),
+                      }}
+                      onClick={header.column.getToggleSortingHandler()}
                     >
-                      {(dir) => <Fa icon={dir() === 'asc' ? faSortUp : faSortDown} class="mx-2" />}
-                    </Show>
-                  </th>
-                )}
-              </For>
-            </tr>
-          )}
-        </For>
-      </thead>
-      <tbody>
-        <For each={table.getRowModel().rows}>
-          {(row, i) => {
-            return (
-              <tr
-                class="border-gray-200 hover:bg-slate-100"
-                classList={{
-                  'border-t': row.depth === 0,
-                  'bg-slate-50': i() % 2 === 1 && row.depth === 0,
-                }}
-              >
-                <For each={row.getVisibleCells()}>
-                  {(cell) => (
-                    <td class="px-4 py-2">
-                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                    </td>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(header.column.columnDef.header, header.getContext())}
+                      <Show
+                        when={header.column.getIsSorted()}
+                        fallback={
+                          <Show when={header.column.getCanSort()}>
+                            <Fa icon={faSort} class="mx-2 text-gray-400" />
+                          </Show>
+                        }
+                      >
+                        {(dir) => (
+                          <Fa icon={dir() === 'asc' ? faSortUp : faSortDown} class="mx-2" />
+                        )}
+                      </Show>
+                    </th>
                   )}
                 </For>
               </tr>
-            )
-          }}
-        </For>
-      </tbody>
-    </table>
+            )}
+          </For>
+        </thead>
+        <tbody>
+          <For each={table.getRowModel().rows}>
+            {(row, i) => {
+              return (
+                <tr
+                  class="border-gray-200 hover:bg-slate-100"
+                  classList={{
+                    'border-t': row.depth === 0,
+                    'bg-slate-50': i() % 2 === 1 && row.depth === 0,
+                  }}
+                >
+                  <For each={row.getVisibleCells()}>
+                    {(cell) => (
+                      <td class="px-4 py-2">
+                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                      </td>
+                    )}
+                  </For>
+                </tr>
+              )
+            }}
+          </For>
+        </tbody>
+      </table>
+    </div>
   )
 }
