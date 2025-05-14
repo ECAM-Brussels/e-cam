a1d99fd8c221c8ec9019f7771b4bfadc52c79636
Khoi Nguyen <khoi@nguyen.me.uk>
2025-05-14 15:01:23 +0200

    chore(table): improve style and subrow support

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -3,26 +3,33 @@ import {
   createSolidTable,
   flexRender,
   getCoreRowModel,
+  getExpandedRowModel,
 } from '@tanstack/solid-table'
 import { For } from 'solid-js'
 
 type TableProps<Row> = {
+  class?: string
   data: Row[]
   columns: ColumnDef<Row>[]
+  subRows?: (row: Row) => Row[] | undefined
 }
 
 export default function Table<Row extends object>(props: TableProps<Row>) {
   const table = createSolidTable({
-    data: props.data,
+    get data() {
+      return props.data
+    },
     columns: props.columns,
+    getSubRows: props.subRows,
     getCoreRowModel: getCoreRowModel(),
+    getExpandedRowModel: getExpandedRowModel(),
   })
   return (
-    <table>
-      <thead>
+    <table class={props.class ?? 'border bg-white text-gray-600 mx-auto my-4'}>
+      <thead class="border text-gray-900 uppercase bg-gray-50">
         <For each={table.getHeaderGroups()}>
           {(headerGroup) => (
-            <tr>
+            <tr class="px-4 py-4 text-sm">
               <For each={headerGroup.headers}>
                 {(header) => (
                   <th>
@@ -38,13 +45,25 @@ export default function Table<Row extends object>(props: TableProps<Row>) {
       </thead>
       <tbody>
         <For each={table.getRowModel().rows}>
-          {(row) => (
-            <tr>
-              <For each={row.getVisibleCells()}>
-                {(cell) => <td>{flexRender(cell.column.columnDef.cell, cell.getContext())}</td>}
-              </For>
-            </tr>
-          )}
+          {(row, i) => {
+            return (
+              <tr
+                class="border-gray-200 hover:bg-slate-100"
+                classList={{
+                  'border-t': row.depth === 0,
+                  'bg-slate-50': i() % 2 === 1 && row.depth === 0,
+                }}
+              >
+                <For each={row.getVisibleCells()}>
+                  {(cell) => (
+                    <td class="px-4 py-2">
+                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    </td>
+                  )}
+                </For>
+              </tr>
+            )
+          }}
         </For>
       </tbody>
     </table>
