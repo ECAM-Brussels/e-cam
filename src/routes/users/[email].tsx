5f64b9e5faee832ee55e89c0fee36225dd2854d6
Khoi Nguyen <khoi@nguyen.me.uk>
2025-05-22 22:21:33 +0200

    feat(graph): group by

--- src/routes/users/[email].tsx
+++ src/routes/users/[email].tsx
@@ -1,13 +1,25 @@
-import { createAsyncStore, useParams } from '@solidjs/router'
-import { Suspense } from 'solid-js'
+import { action, createAsyncStore, useParams } from '@solidjs/router'
+import { createSignal, For, Suspense } from 'solid-js'
 import AssignmentTable from '~/components/AssignmentTable'
 import Graph from '~/components/Graph'
 import Page from '~/components/Page'
 import { getUserInfo } from '~/lib/user'
 
+const grouping = [
+  { label: 'Ne pas grouper', groups: [] },
+  { label: 'Grouper par année', groups: ['4MATH5', '5MATH4', '5MATH6', '6MATH4', '6MATH6'] },
+  { label: 'Grouper par domaine', groups: ['algebra', 'complex'] },
+]
+
 export default function () {
   const params = useParams()
   const user = createAsyncStore(() => getUserInfo(params.email))
+  const [groups, setGroups] = createSignal<string[]>([])
+
+  const groupAssignments = action(async (form: FormData) => {
+    const i = Number(form.get('groupBy'))
+    setGroups(grouping[i].groups)
+  }, 'groupAssignments')
   return (
     <Suspense>
       <Page title={`Profil de ${user()?.firstName} ${user()?.lastName}`}>
@@ -16,7 +28,18 @@ export default function () {
             {user()?.lastName}, {user()?.firstName}
           </h1>
           <AssignmentTable />
-          <Graph class="min-h-96 w-full" />
+          <Graph class="min-h-96 w-full" groups={groups()} />
+          <form method="post" action={groupAssignments} class="flex gap-4">
+            <For each={grouping}>
+              {(group, i) => (
+                <button>
+                  <label>
+                    <input type="radio" name="groupBy" value={i()} /> {group.label}
+                  </label>
+                </button>
+              )}
+            </For>
+          </form>
         </section>
       </Page>
     </Suspense>
