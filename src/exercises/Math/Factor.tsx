b405093948680101ac31d399ae420825f38ba54f
Khoi Nguyen <khoi@nguyen.me.uk>
2025-05-23 18:32:44 +0200

    feat(factor): feedback

--- src/exercises/Math/Factor.tsx
+++ src/exercises/Math/Factor.tsx
@@ -2,14 +2,10 @@ import { sample } from 'lodash-es'
 import { Show } from 'solid-js'
 import { z } from 'zod'
 import Math from '~/components/Math'
+import { graphql } from '~/gql'
 import { createExerciseType } from '~/lib/exercises/base'
-import {
-  checkFactorisation,
-  expand,
-  factor,
-  getFirstRoot,
-  normalizePolynomial,
-} from '~/queries/algebra'
+import { request } from '~/lib/graphql'
+import { checkFactorisation, expand, normalizePolynomial } from '~/queries/algebra'
 
 export function product<T>(...allEntries: T[][]): T[][] {
   return allEntries.reduce<T[][]>(
@@ -37,6 +33,7 @@ const { Component, schema } = createExerciseType({
   question: z
     .object({
       expr: z.string().describe('Expression to factorise'),
+      x: z.string().default('x'),
       expand: z
         .boolean()
         .describe('Whether to expand expr before it is seen by the user')
@@ -51,18 +48,96 @@ const { Component, schema } = createExerciseType({
   attempt: z.string().min(1),
   mark: (question, attempt) => checkFactorisation(attempt, question.expr),
   feedback: [
-    async (remaining, question) => {
-      if (!remaining) {
-        return { answer: await factor(question.expr) }
-      } else {
-        return { root: await getFirstRoot(question.expr) }
+    async (remaining, question, attempt) => {
+      const data = await request(
+        graphql(`
+          query FactorisationFeedback($expr: Math!, $x: Math!, $attempt: Math!) {
+            attempt: expression(expr: $attempt) {
+              isFactored
+              isEqual(expr: $expr)
+              solveset {
+                list {
+                  expr
+                  opposite {
+                    subsIn(expr: $expr, var: $x) {
+                      isEqual(expr: "0")
+                    }
+                  }
+                  subsIn(expr: $expr, var: $x) {
+                    isEqual(expr: "0")
+                  }
+                }
+              }
+            }
+            expr: expression(expr: $expr) {
+              factor {
+                expr
+              }
+              solveset {
+                list {
+                  expr
+                  opposite {
+                    add(expr: $x) {
+                      expr
+                    }
+                  }
+                }
+              }
+            }
+          }
+        `),
+        { attempt: attempt, expr: question.expr, x: question.x },
+      )
+      const studentRoots = data.attempt.solveset.list.map((info) => ({
+        root: info.expr,
+        opposite: info.opposite.subsIn.isEqual === true,
+        correct: info.subsIn.isEqual === true,
+      }))
+      return {
+        remaining,
+        answer: data.expr.factor.expr,
+        factors: data.expr.solveset.list.map((root) => root.opposite.add.expr),
+        firstCorrectRoot: data.expr.solveset.list[0].expr,
+        correctRoots: studentRoots.filter((r) => r.correct),
+        wrongSign: studentRoots.filter((r) => r.opposite),
+        incorrectRoots: studentRoots.filter((r) => !r.correct),
+        question,
       }
     },
     (props) => (
-      <Show when={props.answer} fallback={<p>Une des racines est {props.root}</p>}>
-        <p>
-          La réponse est <Math value={props.answer} />
-        </p>
+      <Show
+        when={props.remaining}
+        fallback={
+          <div>
+            <p>
+              On commence par trouver la racine{' '}
+              <Math value={`${props.question.x} = ${props.firstCorrectRoot}`} /> par essais et
+              erreurs, donc <Math value={props.factors[0]} /> est un facteur.
+            </p>
+            <p>
+              Via la distributivité, on atteint la réponse finale <Math value={props.answer} />.
+            </p>
+          </div>
+        }
+      >
+        <Show when={props.correctRoots.length === 0}>
+          <Show
+            when={props.wrongSign.length}
+            fallback={<p>Est-ce que tu peux trouver une racine à vue?</p>}
+          >
+            <p>Vérifie le signe de tes racines.</p>
+          </Show>
+          <p>
+            Rappelle-toi que si <Math value={`${props.question.x} = a`} /> est une racine, alors{' '}
+            <Math value={`${props.question.x} - a`} /> est un facteur.
+          </p>
+        </Show>
+        <Show when={props.correctRoots.length === 1 && props.incorrectRoots}>
+          <p>
+            La racine <Math value={`${props.question.x} = ${props.correctRoots[0].root}`} /> est
+            correcte. Peux-tu vérifier ta distributivité?
+          </p>
+        </Show>
       </Show>
     ),
   ],
@@ -73,6 +148,7 @@ const { Component, schema } = createExerciseType({
         .array()
         .default([1])
         .describe('Possibilities for the constant factor in the factorisation'),
+      X: z.string().array().default(['x']),
       roots: z
         .union([
           math.array().array(),
