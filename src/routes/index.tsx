342bd2d522fb2c114698d3091e4233fb9204f3cf
Khoi Nguyen <khoi@nguyen.me.uk>
2025-05-26 16:57:41 +0200

    content(home): add graph

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,8 @@
 import { createAsync, type RouteDefinition } from '@solidjs/router'
-import { For, Show, type JSXElement } from 'solid-js'
+import { createSignal, For, Show, type JSXElement } from 'solid-js'
 import Graph from '~/components/Graph'
 import Page from '~/components/Page'
-import { getUser } from '~/lib/auth/session'
+import { getSession, getUser } from '~/lib/auth/session'
 import { getAssignmentList } from '~/lib/exercises/assignment'
 
 export const route = {
@@ -12,12 +12,42 @@ export const route = {
   },
 } satisfies RouteDefinition
 
+const options = {
+  PM1C: "Je suis étudiant ou considère étudier à l'ECAM",
+  '5MATH4': 'Je suis en 5ème secondaire',
+  '6MATH4': 'Je suis en 6ème secondaire',
+} as const
+
 export default function Home() {
+  const [choice, setChoice] = createSignal<keyof typeof options | undefined>(undefined)
   return (
     <Page title="Accueil" class="">
-      <div class="bg-white">
-        <Graph class="container mx-auto h-[600px]" groups={['algebra', 'calculus']} />
+      <div class="mx-auto h-[400px] flex flex-col justify-center text-center bg-gradient-to-b from-slate-50 to-sky-50">
+        <h2 class="text-3xl my-8">Que souhaitez-vous réviser?</h2>
+        <div class="md:flex justify-center">
+          <For each={Object.entries(options)}>
+            {([course, label]) => (
+              <button
+                class="block rounded-full border shadow-sm p-4 m-4 bg-white hover:ring-blue-400 hover:ring-2"
+                classList={{
+                  'ring-sky-400 ring-2': choice() === course,
+                }}
+                onClick={() => setChoice(course as keyof typeof options)}
+              >
+                {label}
+              </button>
+            )}
+          </For>
+        </div>
       </div>
+      <Show when={choice()}>
+        <div class="bg-white">
+          <Graph
+            class="container mx-auto h-[600px]"
+            query={{ courses: { some: { code: choice() } } }}
+          />
+        </div>
+      </Show>
       <h2 class="text-4xl font-bold text-slate-800 my-8 container mx-auto">Compétences</h2>
       <Skills title="Algèbre" code="algebra" open />
       <Skills title="Analyse" code="calculus" open />
