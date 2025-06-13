ccb17df3150022d1a091cbc36f7567c07ef2191e
Khoi Nguyen <khoi@nguyen.me.uk>
2025-06-13 19:52:08 +0200

    Improve type safety for createSearchParam hook

--- src/lib/params.ts
+++ src/lib/params.ts
@@ -1,14 +1,11 @@
 import { useSearchParams } from '@solidjs/router'
 import { z } from 'zod'
 
-export function createSearchParam(
-  name: string,
-  schema: z.ZodTypeAny = z.coerce.string().default(''),
-) {
+export function createSearchParam<S extends z.ZodTypeAny>(name: string, schema: S) {
   const defaultVal = schema.parse(undefined)
   const [searchParams, setSearchParams] = useSearchParams()
   return [
-    () => schema.parse(searchParams[name]),
+    () => schema.parse(searchParams[name]) as z.infer<S>,
     (newValue: z.infer<typeof schema>) => {
       setSearchParams({ [name]: newValue === defaultVal ? undefined : newValue })
     },
