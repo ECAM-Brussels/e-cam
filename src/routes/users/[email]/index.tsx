40ba9dc20f104fb5419e9c9126a3ab2672283233
Khoi Nguyen <khoi@nguyen.me.uk>
2025-06-20 14:29:53 +0200

    Fix suspense that was too high in the component tree

--- src/routes/users/[email]/index.tsx
+++ src/routes/users/[email]/index.tsx
@@ -1,6 +1,5 @@
 import UserTabs from './_tabs'
 import { createAsyncStore, type RouteDefinition, useParams } from '@solidjs/router'
-import { Suspense } from 'solid-js'
 import AssignmentTable from '~/components/AssignmentTable'
 import Page from '~/components/Page'
 import { getUser } from '~/lib/auth/session'
@@ -19,16 +18,14 @@ export default function () {
   const params = useParams()
   const user = createAsyncStore(() => getUserInfo(params.email))
   return (
-    <Suspense>
-      <Page title={`Profil de ${user()?.firstName} ${user()?.lastName}`}>
-        <UserTabs />
-        <section class="bg-white rounded-xl p-4 py-8 border">
-          <h1 class="font-bold text-3xl my-8">
-            {user()?.lastName}, {user()?.firstName}
-          </h1>
-          <AssignmentTable search />
-        </section>
-      </Page>
-    </Suspense>
+    <Page title={`Profil de ${user()?.firstName} ${user()?.lastName}`}>
+      <UserTabs />
+      <section class="bg-white rounded-xl p-4 py-8 border">
+        <h1 class="font-bold text-3xl my-8">
+          {user()?.lastName}, {user()?.firstName}
+        </h1>
+        <AssignmentTable search />
+      </section>
+    </Page>
   )
 }
