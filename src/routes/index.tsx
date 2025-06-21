2b5de7fc97f4bedf0e4849702a0e8c431caa711e
Khoi Nguyen <khoi@nguyen.me.uk>
2025-06-21 16:39:01 +0200

    Hide form when post request is in flight

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
-import { createAsyncStore, type RouteDefinition } from '@solidjs/router'
-import { For } from 'solid-js'
+import { createAsyncStore, useSubmission, type RouteDefinition } from '@solidjs/router'
+import { For, Show } from 'solid-js'
 import Card from '~/components/Card'
 import Page from '~/components/Page'
 import { getPreferences, getUser, setPreferences } from '~/lib/auth/session'
@@ -17,24 +17,30 @@ export default function Home() {
   const courses = createAsyncStore(() =>
     getCourses({ where: { url: { not: '' }, image: { not: '' } } }),
   )
+  const settingPreference = useSubmission(setPreferences)
   return (
     <Page title="Accueil">
-      <form action={setPreferences.with({ ecam: false })} method="post">
-        <label class="inline-flex gap-4">
-          <input
-            type="checkbox"
-            class="sr-only peer"
-            name="ecam"
-            checked={preferences()?.ecam}
-            onChange={(e) => {
-              e.target.closest('form')?.requestSubmit()
-            }}
-          />{' '}
-          <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600 dark:peer-checked:bg-blue-600"></div>
-          Je suis étudiant·e à l'ECAM
-        </label>
-        <label class="inline-flex items-center cursor-pointer"></label>
-      </form>
+      <Show
+        when={!settingPreference.pending}
+        fallback={<p>Changement de la préférence en cours...</p>}
+      >
+        <form action={setPreferences.with({ ecam: false })} method="post">
+          <label class="inline-flex gap-4">
+            <input
+              type="checkbox"
+              class="sr-only peer"
+              name="ecam"
+              checked={preferences()?.ecam}
+              onChange={(e) => {
+                e.target.closest('form')?.requestSubmit()
+              }}
+            />{' '}
+            <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600 dark:peer-checked:bg-blue-600"></div>
+            Je suis étudiant·e à l'ECAM
+          </label>
+          <label class="inline-flex items-center cursor-pointer"></label>
+        </form>
+      </Show>
       <div class="grid lg:grid-cols-2 gap-8 container mx-auto my-8">
         <For each={courses()?.filter((c) => c.ecam === preferences()?.ecam)}>
           {(course) => (
