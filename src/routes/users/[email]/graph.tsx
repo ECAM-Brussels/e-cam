20a3dd49573f3d2394f5cb1dd9e32495a1aecaea
Khoi Nguyen <khoi@nguyen.me.uk>
2025-06-22 22:32:43 +0200

    Sort by field by default

--- src/routes/users/[email]/graph.tsx
+++ src/routes/users/[email]/graph.tsx
@@ -1,10 +1,12 @@
 import UserTabs from './_tabs'
-import { action, type RouteDefinition } from '@solidjs/router'
+import { action, json, type RouteDefinition } from '@solidjs/router'
 import { createSignal, For } from 'solid-js'
+import { z } from 'zod'
 import Graph from '~/components/Graph'
 import Page from '~/components/Page'
 import { getUser } from '~/lib/auth/session'
 import { getAssignmentGraph } from '~/lib/exercises/assignment'
+import { createSearchParam } from '~/lib/params'
 
 export const route = {
   preload() {
@@ -13,33 +15,41 @@ export const route = {
   },
 } satisfies RouteDefinition
 
-const grouping = [
-  { label: 'Ne pas grouper', groups: [] },
-  { label: 'Grouper par année', groups: ['4MATH5', '5MATH4', '5MATH6', '6MATH4', '6MATH6'] },
-  { label: 'Grouper par domaine', groups: ['algebra', 'calculus', 'trigonometry', 'geometry'] },
-]
+const groupsSchema = z
+  .union([z.literal('none'), z.literal('year'), z.literal('field')])
+  .default('field')
+
+const grouping = {
+  none: { label: 'Ne pas grouper', groups: [] },
+  year: { label: 'Grouper par année', groups: ['4MATH5', '5MATH4', '5MATH6', '6MATH4', '6MATH6'] },
+  field: {
+    label: 'Grouper par domaine',
+    groups: ['algebra', 'calculus', 'trigonometry', 'geometry'],
+  },
+}
 
 export default function () {
-  const [groups, setGroups] = createSignal<string[]>([])
+  const [groupBy, setGroupBy] = createSearchParam('groupBy', groupsSchema)
   const groupAssignments = action(async (form: FormData) => {
-    setGroups(grouping[Number(form.get('groupBy'))].groups)
+    setGroupBy(groupsSchema.parse(form.get('groupBy')))
+    return json(null, { revalidate: 'nothing' })
   }, 'groupAssignments')
   return (
     <Page title="Progrès">
       <UserTabs />
       <section class="bg-white rounded-xl p-4 py-8 border">
         <form method="post" action={groupAssignments} class="flex gap-4">
-          <For each={grouping}>
-            {(group, i) => (
+          <For each={Object.entries(grouping)}>
+            {([key, group]) => (
               <button>
                 <label>
-                  <input type="radio" name="groupBy" value={i()} /> {group.label}
+                  <input type="radio" name="groupBy" value={key} /> {group.label}
                 </label>
               </button>
             )}
           </For>
         </form>
-        <Graph class="min-h-96 w-full h-screen" groups={groups()} />
+        <Graph class="min-h-96 w-full h-screen" groups={grouping[groupBy()].groups} />
       </section>
     </Page>
   )
