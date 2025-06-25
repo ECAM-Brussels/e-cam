d8cca4047e1458c7d42e80aaa84fb07a865e2d21
Khoi Nguyen <khoi@nguyen.me.uk>
2025-06-25 08:39:21 +0200

    Only show attempts with ELO

--- src/lib/exercises/attempt.ts
+++ src/lib/exercises/attempt.ts
@@ -4,7 +4,7 @@ import { prisma } from '~/lib/db'
 export const getUserAttempts = query(async (email: string) => {
   'use server'
   const data = await prisma.attempt.findMany({
-    where: { email, correct: { not: null } },
+    where: { email, correct: { not: null }, gain: { not: null } },
     orderBy: { date: 'desc' },
     include: { question: true, assignment: { include: { page: true } } },
   })
