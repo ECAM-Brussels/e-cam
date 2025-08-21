c83f01cf9039efa9cb12e9589f1aa90bb5782017
Khoi Nguyen <khoi@nguyen.me.uk>
2025-08-21 17:32:30 +0200

    Add feedback for Pythagoras

--- src/exercises/Math/Pythagoras.tsx
+++ src/exercises/Math/Pythagoras.tsx
@@ -1,9 +1,11 @@
 import { random, sample, sampleSize } from 'lodash-es'
+import { Show } from 'solid-js'
 import { z } from 'zod'
 import Math from '~/components/Math'
 import RightTriangle from '~/components/RightTriangle'
 import { createExerciseType } from '~/lib/exercises/base'
-import { checkEqual } from '~/queries/algebra'
+import { narrow } from '~/lib/helpers'
+import { checkEqual, simplify } from '~/queries/algebra'
 
 const math = z.string().nonempty().or(z.number().transform(String))
 
@@ -42,6 +44,30 @@ const { Component, schema } = createExerciseType({
     data[question.unknown] = attempt ?? ''
     return await checkEqual(`(${data.a})^2 + (${data.b})^2 - (${data.c})^2`, '0')
   },
+  feedback: [
+    async (remaining, question) => {
+      'use server'
+      if (!remaining) {
+        const eqs = {
+          a: `\\sqrt{(${question.c})^2 - (${question.b})^2}`,
+          b: `\\sqrt{(${question.c})^2 - (${question.a})^2}`,
+          c: `\\sqrt{(${question.a})^2 + (${question.b})^2}`,
+        } as const
+        return { remaining, ...question, answer: await simplify(eqs[question.unknown]) }
+      }
+      return { remaining, ...question }
+    },
+    (props) => (
+      <Show
+        when={narrow(
+          () => props,
+          (p) => 'answer' in p,
+        )}
+      >
+        {(props) => <Math value={`${props()[props().unknown]} = ${props().answer}`} displayMode />}
+      </Show>
+    ),
+  ],
   generator: {
     params: z.object({
       N: z.number(),
