f11c704336ac50712212aef8301f0b5886739141
Khoi Nguyen <khoi@nguyen.me.uk>
2025-09-04 16:35:07 +0200

    Flex wrap

--- src/components/ChapterInfo.tsx
+++ src/components/ChapterInfo.tsx
@@ -1,4 +1,3 @@
-import Fa from './Fa'
 import Graph from './Graph'
 import { type JSXElement, Show } from 'solid-js'
 
@@ -12,7 +11,7 @@ export default function ChapterInfo(props: ChapterInfoProps) {
   return (
     <div class="bg-white border rounded-xl px-4 mb-2 pb-4">
       <h3 class="px-4 font-bold text-xl">{props.title}</h3>
-      <div class="md:flex gap-4 text-sm">{props.children}</div>
+      <div class="md:flex md:flex-wrap gap-4 text-sm">{props.children}</div>
       <Show when={props.query}>
         <Graph class="mt-2 h-96 border rounded-xl" query={props.query} />
       </Show>
