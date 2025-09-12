bb134f21a8de9d425130e0ef0c67f609181d6d99
Khoi Nguyen <khoi@nguyen.me.uk>
2025-09-12 13:16:53 +0200

    Improve triangle

--- src/components/RightTriangle.tsx
+++ src/components/RightTriangle.tsx
@@ -8,17 +8,19 @@ type Props = {
   class?: string
   width: number
   height: number
+  A?: string
+  B?: string
+  C?: string
 }
 
 export default function RightTriangle(props: Props) {
-  const margin = 30
+  const margin = 50
   const A = () => [margin, props.height - margin]
   const B = () => [props.width - margin, props.height - margin]
   const C = () => [props.width - margin, margin]
   return (
     <div class={props.class} classList={{ relative: true }}>
       <svg
-        class="bg-white"
         width={props.width}
         height={props.height}
         viewBox={`0 0 ${props.width} ${props.height}`}
@@ -33,16 +35,39 @@ export default function RightTriangle(props: Props) {
       </svg>
       <Label y={props.height - margin} x={props.width / 2} tex={props.a} />
       <Label y={props.height / 2} x={props.width - margin} tex={props.b} />
-      <Label y={props.height / 2} x={props.width / 2} tex={props.c} />
+      <Label
+        class="-translate-x-full -translate-y-full"
+        y={props.height / 2}
+        x={props.width / 2}
+        tex={props.c}
+      />
+      <Label
+        class="-translate-x-full translate-y-1/2"
+        y={margin}
+        x={props.width - margin}
+        tex={props.A}
+      />
+      <Label
+        class="-translate-y-full translate-x-1/2"
+        y={props.height - margin}
+        x={margin}
+        tex={props.B}
+      />
+      <Label
+        class="-translate-x-full -translate-y-full"
+        y={props.height - margin}
+        x={props.width - margin}
+        tex={props.C}
+      />
     </div>
   )
 }
 
-function Label(props: { x: number; y: number; tex?: string }) {
+function Label(props: { class?: string; x: number; y: number; tex?: string }) {
   return (
     <Show when={props.tex}>
       <div
-        class="absolute -translate-y-1/2 -translate-x-1/2 bg-white p-2"
+        class={`absolute p-2 ${props.class}`}
         style={{
           top: `${props.y}px`,
           left: `${props.x}px`,
