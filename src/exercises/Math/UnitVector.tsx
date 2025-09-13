edf46ca3ae6d63a08381f461a3649c813c034172
Khoi Nguyen <khoi@nguyen.me.uk>
2025-09-13 13:27:18 +0200

    Fix unit vector exercise

--- src/exercises/Math/UnitVector.tsx
+++ src/exercises/Math/UnitVector.tsx
@@ -1,22 +1,58 @@
 import { sample } from 'lodash-es'
+import { Show } from 'solid-js'
+import { createStore } from 'solid-js/store'
 import { z } from 'zod'
 import Math from '~/components/Math'
 import { graphql } from '~/gql'
-import { createExerciseType } from '~/lib/exercises/base'
+import { createExerciseType, useExerciseContext } from '~/lib/exercises/base'
 import { request } from '~/lib/graphql'
+import { narrow } from '~/lib/helpers'
 
 const vector = z.string().nonempty().or(z.number().transform(String)).array().nonempty()
 
+const vect = (v: string[]) => String.raw`\begin{pmatrix} ${v.join(' \\\\ ')} \\ \end{pmatrix}`
+
 const { Component, schema } = createExerciseType({
   name: 'UnitVector',
-  Component: (props) => (
-    <>
-      <p>
-        Calculez le <strong>vecteur unitaire</strong> associé à{' '}
-        <Math value={`\\left(${props.question.v.join(';')}\\right)`} />
-      </p>
-    </>
-  ),
+  Component: (props) => {
+    const [c, setC] = createStore<string[]>(props.attempt ?? ['', '', ''])
+    const exercise = useExerciseContext()
+    return (
+      <>
+        <p>
+          Calculez le <strong>vecteur unitaire</strong> associé à{' '}
+          <Math value={`\\vec v = \\left(${props.question.v.join(';')}\\right)`} />
+        </p>
+        <p class="flex justify-center items-center gap-2">
+          <Math value={`\\hat v =`} displayMode />
+          <Math
+            value={
+              exercise?.readOnly
+                ? vect(props.attempt ?? [])
+                : vect([
+                    String.raw`\placeholder[c1]{${props.attempt?.[0] ?? ''}}`,
+                    String.raw`\placeholder[c2]{${props.attempt?.[1] ?? ''}}`,
+                    String.raw`\placeholder[c3]{${props.attempt?.[2] ?? ''}}`,
+                  ])
+            }
+            editable
+            displayMode
+            readOnly
+            onBlur={(e) => {
+              setC([
+                e.target.getPromptValue('c1'),
+                e.target.getPromptValue('c2'),
+                e.target.getPromptValue('c3'),
+              ])
+            }}
+          />
+          <input type="hidden" name="attempt" value={c[0]} />
+          <input type="hidden" name="attempt" value={c[1]} />
+          <input type="hidden" name="attempt" value={c[2]} />
+        </p>
+      </>
+    )
+  },
   question: z.object({
     v: vector,
   }),
@@ -50,6 +86,39 @@ const { Component, schema } = createExerciseType({
     )
     return vector.angle.isEqual && vector.norm.isEqual
   },
+  feedback: [
+    async (remaining, question) => {
+      'use server'
+      const { vector } = await request(
+        graphql(`
+          query GetUnitVector($v: [Math!]!) {
+            vector(coordinates: $v) {
+              unitVector {
+                expr {
+                  expr
+                }
+              }
+            }
+          }
+        `),
+        question,
+      )
+      if (!remaining) {
+        return { remaining, ...question, answer: vector.unitVector.expr.map((c) => c.expr) }
+      }
+      return { remaining }
+    },
+    (props) => (
+      <Show
+        when={narrow(
+          () => props,
+          (p) => 'answer' in p,
+        )}
+      >
+        {(p) => <Math value={`\\hat v = ${vect(p().answer)}`} displayMode />}
+      </Show>
+    ),
+  ],
   generator: {
     params: z.object({
       N: z.number().array().nonempty().default([3]),
