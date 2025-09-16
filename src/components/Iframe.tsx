bfd5d558e9aaa9ecf74a388d33f1ac04edf7c11d
Khoi Nguyen <khoi@nguyen.me.uk>
2025-09-16 17:08:08 +0200

    Change iframe for PDFs

--- src/components/Iframe.tsx
+++ src/components/Iframe.tsx
@@ -4,10 +4,19 @@ type IframeProps = {
   loading?: 'lazy' | 'eager'
 }
 export default function Iframe(props: IframeProps) {
+  const src = () => {
+    const safari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent)
+    const pdf = /\.pdf($|[?#])/i.test(props.src)
+    if (safari && pdf) {
+      const [base, hash] = props.src.split('#')
+      return `https://mozilla.github.io/pdf.js/web/viewer.html?file=${encodeURIComponent(base)}${hash ? `#${hash}` : ''}`
+    }
+    return props.src
+  }
   return (
     <iframe
       loading={props.loading || 'lazy'}
-      src={props.src}
+      src={src()}
       class={props.class}
       classList={{
         relative: true,
