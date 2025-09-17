55a74316dfdd48938a103e9af9993f923792e195
Khoi Nguyen <khoi@nguyen.me.uk>
2025-09-17 08:56:56 +0200

    Fix ressources on smartphone

--- src/components/ChapterInfo.tsx
+++ src/components/ChapterInfo.tsx
@@ -11,7 +11,7 @@ export default function ChapterInfo(props: ChapterInfoProps) {
   return (
     <div class="bg-white border rounded-xl px-4 mb-2 pb-4">
       <h3 class="px-4 font-bold text-xl">{props.title}</h3>
-      <div class="md:flex md:flex-wrap gap-4 text-sm">{props.children}</div>
+      <div class="flex flex-wrap gap-2 md:gap-4 text-sm">{props.children}</div>
       <Show when={props.query}>
         <Graph class="mt-2 h-96 border rounded-xl" query={props.query} />
       </Show>
