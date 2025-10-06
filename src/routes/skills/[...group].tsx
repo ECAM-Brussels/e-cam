b62ae4c7cc1083b204a384ee698255666f178a2a
Khoi Nguyen <khoi@nguyen.me.uk>
2025-10-06 19:06:12 +0200

    Improve navigation

--- src/routes/skills/[...group].tsx
+++ src/routes/skills/[...group].tsx
@@ -1,12 +1,23 @@
-import { useLocation } from '@solidjs/router'
+import { createAsync, query, useLocation } from '@solidjs/router'
 import Graph from '~/components/Graph'
 import Page from '~/components/Page'
+import { prisma } from '~/lib/db'
+
+const getPageInfo = query(async (url: string) => {
+  'use server'
+  return await prisma.page.findUnique({ where: { url } })
+}, 'getPageInfo')
 
 export default function () {
   const location = useLocation()
+  const info = createAsync(() => getPageInfo(location.pathname))
   return (
-    <Page title="Hello">
-      <Graph class="min-h-[600px]" query={{ url: { startsWith: location.pathname } }} />
+    <Page title={info()?.title}>
+      <h1 class="text-3xl font-bold mb-4">{info()?.title}</h1>
+      <Graph
+        class="bg-white border rounded-xl h-[900px]"
+        query={{ url: { startsWith: location.pathname } }}
+      />
     </Page>
   )
 }
