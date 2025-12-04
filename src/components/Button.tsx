7fb3bc3e0466acc34d1a3a981e8c31f0701c7d81
Khoi Nguyen <khoi@nguyen.me.uk>
2025-12-04 15:23:30 +0100

    Change button z-index

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -11,7 +11,7 @@ export default function Button(rawProps: ButtonProps) {
     <button
       classList={{
         'disabled:opacity-50 disabled:cursor-not-allowed': true,
-        'rounded-lg py-2 px-4 shadow font-bold': true,
+        'rounded-lg py-2 px-4 shadow font-bold z-50': true,
         border: props.variant === 'outlined',
         'bg-white hover:bg-gray-100': props.color === 'white',
         'bg-green-800 hover:bg-green-900 text-green-50': props.color === 'green',
