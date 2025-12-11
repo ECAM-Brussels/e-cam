f7a1060dc392b2b7de7003f4c475254ec2a486dd
Khoi Nguyen <khoi@nguyen.me.uk>
2025-12-11 22:32:02 +0100

    Simplify schema and improve type safety

--- src/lib/exercises/type.tsx
+++ src/lib/exercises/type.tsx
@@ -1,69 +1,91 @@
-import type { JSX } from 'solid-js'
+import { extractFormData } from '../form'
+import { action, createAsync, query } from '@solidjs/router'
+import { createEffect, createSignal, type JSX } from 'solid-js'
 import z from 'zod/v4'
+import ErrorBoundary from '~/components/ErrorBoundary'
+import Suspense from '~/components/Suspense'
 
-export type StepTemplate = z.ZodDiscriminatedUnion<
-  z.ZodObject<{ name: z.ZodLiteral; state: z.ZodObject }>[]
->
-export type Schema<Q = z.ZodObject, S = StepTemplate, P = z.ZodObject | undefined> = {
-  question: Q
-  steps: S
-  params?: P
+export type Schema = {
+  name: string
+  question: z.ZodObject
+  steps: Record<string, z.ZodObject>
+  params?: z.ZodObject
 }
 
-type Unwrap<T> = T extends (infer U)[] ? U : T
-type StepSchema<Q, S, P> = Unwrap<z.infer<Schema<Q, S, P>['steps']>>
-type Step<Q, S, K, F> = {
-  feedback: (question: Q, state: S) => Promise<{ correct: boolean; next?: K; feedback: F }>
+type AttemptSchema<T extends Schema> = {
+  [K in keyof T['steps']]: z.ZodObject<{
+    name: z.ZodLiteral<K & string>
+    state: T['steps'][K & string]
+  }>
+}[keyof T['steps']]
+
+type FullSchema<T extends Schema> = z.ZodObject<{
+  question: T['question']
+  attempt: z.ZodArray<z.ZodUnion<AttemptSchema<T>[]>>
+}>
+
+export function buildSchema<T extends Schema>(schema: T): FullSchema<T> {
+  return z.object({
+    question: schema.question,
+    attempt: z
+      .union(
+        Object.entries(schema.steps).map(([name, state]) =>
+          z.object({ name: z.literal(name), state }),
+        ) as AttemptSchema<T>[],
+      )
+      .array(),
+  })
+}
+
+type Step<Q, S, F> = {
+  feedback: (question: Q, state: S) => Promise<F>
   View: (props: { question: Q; state?: S; feedback?: F }) => JSX.Element
 }
 
-export function defineStep<Q, S extends StepTemplate, P, N extends StepSchema<Q, S, P>['name'], F>(
-  _schema: Schema<Q, S, P> & Schema,
-  _stepName: N,
+export function defineStep<T extends Schema, N extends keyof T['steps'] & string, F>(
+  schema: T,
+  name: N,
   step: Step<
-    z.infer<Q>,
-    Extract<StepSchema<Q, S, P>, { name: N }>['state'],
-    StepSchema<Q, S, P>['name'],
-    F
+    z.infer<T['question']>,
+    z.infer<T['steps'][N]>,
+    { correct: boolean; next: keyof T['steps'] | null; data: F }
   >,
 ) {
-  return step
+  return { ...step, name, schema, stepSchema: schema.steps[name] as T['steps'][N] }
 }
 
-export function defineGenerator<Q, P>(
-  _schema: Schema<Q, StepTemplate, P> & Schema,
-  fn: (params: { [K in keyof z.infer<P>]: Unwrap<z.infer<P>[K]> }) => Promise<z.infer<Q>>,
+export function buildStep<T extends Schema, N extends keyof T['steps'] & string, F>(
+  step: ReturnType<typeof defineStep<T, N, F>>,
 ) {
-  return fn
+  const getFeedback = query(step.feedback, `exercise-${step.schema.name}-${step.name}`)
+  return function (props: {
+    question: z.infer<T['question']>
+    state?: z.infer<T['steps'][N]>
+    onChange?: (
+      newState: z.infer<T['steps'][N]>,
+      feedback: Awaited<ReturnType<typeof step.feedback>>,
+    ) => Promise<void>
+  }) {
+    const [state, setState] = createSignal(props.state)
+    createEffect(() => setState(() => props.state))
+    const feedback = createAsync(async () => {
+      if (state()) return getFeedback(props.question, state()!)
+    })
+    const submit = action(async (form: FormData) => {
+      const newState = await step.stepSchema.parseAsync(extractFormData(form))
+      const feedback = await getFeedback(props.question, newState)
+      await props.onChange?.(newState, feedback)
+      setState(() => newState)
+      return feedback
+    })
+    return (
+      <form method="post" action={submit}>
+        <Suspense>
+          <ErrorBoundary>
+            <step.View question={props.question} state={state()} feedback={feedback()} />
+          </ErrorBoundary>
+        </Suspense>
+      </form>
+    )
+  }
 }
-
-export const schema = {
-  question: z.object({ expr: z.string() }),
-  steps: z.discriminatedUnion('name', [
-    z.object({
-      name: z.literal('start'),
-      state: z.object({ attempt: z.string() }),
-    }),
-    z.object({
-      name: z.literal('root'),
-      state: z.object({ root: z.string() }),
-    }),
-  ]),
-  params: z.object({
-    x1: z.string().array(),
-  }),
-} satisfies Schema
-
-const generator = defineGenerator(schema, async ({ x1 }) => {
-  return { expr: `(x - ${x1}) (x - ${x1})` }
-})
-
-export const start = defineStep(schema, 'start', {
-  async feedback(question, state) {
-    const correct = state.attempt === 'hello'
-    return { correct: true, feedback: { hello: 'world' } }
-  },
-  View(props) {
-    return <p>Hello world {props.feedback?.hello}</p>
-  },
-})
